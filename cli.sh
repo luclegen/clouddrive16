e297939af32e77c19c39f6816491254b705e70b6
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-04 23:54:53 +0700

    Create command-line interface

---
+++ cli.sh
@@ -0,0 +1,11 @@
+# COMMAND
+
+# * Prerequisites *
+sudo npm i -g npm nodemon
+
+# I. REACT CLIENT
+npx create-react-app client
+cd client; npm i -s node-sass axios bootstrap@4.6 reactstrap@8.9 --legacy-peer-deps
+
+# II. EXPRESS.JS SERVER
+clear; npm start
