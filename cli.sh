b11958ba2ec344f38074c724368b5351d77046f4
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-05 23:04:33 +0700

    Update cli

--- cli.sh
+++ cli.sh
@@ -4,8 +4,10 @@
 sudo npm i -g npm nodemon
 
 # I. REACT CLIENT
+clear; npm start
 npx create-react-app client
 cd client; npm i -s node-sass axios bootstrap@4.6 reactstrap@8.9 --legacy-peer-deps
 
 # II. EXPRESS.JS SERVER
 clear; npm start
+npm i -s dotenv mongoose express body-parser bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer rimraf
