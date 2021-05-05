779b631b5d775ee2937d12aff3a8674827165864
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-05 22:54:06 +0700

    Create server app

---
+++ server/src/app.js
@@ -0,0 +1,9 @@
+// Environment Variables
+require('dotenv').config();
+
+// Initialization
+const app = require('express')()
+
+app.get('/', (req, res) => res.send(`Hello ${process.env.SERVER_NAME}!`))
+
+app.listen(process.env.PORT, () => console.log(`Server started at port: http://localhost:${process.env.PORT || 5000}`))
