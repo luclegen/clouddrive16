9550ea0962cc711726a09ddc33389c77a097a363
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-07 23:03:42 +0700

    Update Header component

--- client/src/app/components/Header.js
+++ client/src/app/components/Header.js
@@ -1,12 +1,20 @@
 import { Component } from 'react'
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isHover: false
+    }
+  }
+
   render() {
     return (
       <header>
         <nav>
-          <a className="logo" href="/">
-            <img src="logo.png" alt="Logo" />
+          <a className="logo" href="/" onMouseEnter={this.toggle} onMouseLeave={this.toggle}>
+            <img src={'logo' + (this.state.isHover ? '.hover' : '') + '.png'} alt="Logo" />
             {process.env.REACT_APP_CLIENT_NAME}
           </a>
         </nav>
