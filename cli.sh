50aea040afd5232829a1084cf371ed9a3248d754
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-07 11:59:02 +0700

    Add react-router-dom to cli

--- cli.sh
+++ cli.sh
@@ -6,7 +6,7 @@ sudo npm i -g npm nodemon
 # I. REACT CLIENT
 clear; npm start
 npx create-react-app client
-cd client; npm i -s node-sass axios bootstrap@4.6 reactstrap@8.9 --legacy-peer-deps
+cd client; npm i -s node-sass react-router-dom axios bootstrap@4.6 reactstrap@8.9 --legacy-peer-deps
 
 # II. EXPRESS.JS SERVER
 clear; npm start
