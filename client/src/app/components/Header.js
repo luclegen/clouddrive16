0d90ef1aa2181801a24b86881dbec8c1b369a5d7
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-07 17:30:12 +0700

    Update Header component

--- client/src/app/components/Header.js
+++ client/src/app/components/Header.js
@@ -3,7 +3,14 @@ import { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <header>Header</header>
+      <header>
+        <nav>
+          <a className="logo" href="/">
+            <img src="logo.png" alt="Logo" />
+            {process.env.REACT_APP_CLIENT_NAME}
+          </a>
+        </nav>
+      </header>
     )
   }
 }
