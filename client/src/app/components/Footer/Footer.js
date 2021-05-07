e34ed34d8436e1bd59f301131945168a154dbdb3
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-07 16:40:02 +0700

    Create Footer component

---
+++ client/src/app/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.sass'
+import { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return(
+      <footer>
+        Footer
+      </footer>
+    )
+  }
+}
