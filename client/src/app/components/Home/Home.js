2a31791138beda1cbd32caafb3170502b2f0c801
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-07 16:55:25 +0700

    Update Home component

--- client/src/app/components/Home/Home.js
+++ client/src/app/components/Home/Home.js
@@ -4,7 +4,7 @@ import { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <main className="Main">
+      <main>
         Home
       </main>
     )
