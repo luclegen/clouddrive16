46f360d88a226521ff5f522c8646af3811a6c7ba
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-07 10:57:52 +0700

    Setup components

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,21 @@
+const express = require('express')
+
 // Environment Variables
-require('dotenv').config();
+require('dotenv').config()
 
 // Initialization
-const app = require('express')()
+const app = express()
 
-app.get('/', (req, res) => res.send(`Hello ${process.env.SERVER_NAME}!`))
+// Middlewares
+app.use(require('cors')())
+app.use(express.json())
+app.use(require('passport').initialize())
 
+// Notification
+app.get('/', (req, res) => res.send(`Started ${process.env.SERVER_NAME} server is successfuly!`))
+
+// Error handle
+app.use((err, req, res, next) => res.status(442).send(err.name == 'ValidationError' ? { msg: Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(', ') + '!' } : { error: err, msg: 'Unknown error!' }))
+
+// Start server
 app.listen(process.env.PORT, () => console.log(`Server started at http://localhost:${process.env.PORT || 5000}`))
