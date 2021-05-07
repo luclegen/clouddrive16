13076a7e013a0d2c745e435173377747d33081f7
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-07 16:41:06 +0700

    Add Home component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,12 +1,16 @@
 import './App.sass'
+import { Component } from "react";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Route path="/" component={Home} />
+      </Router>
+    )
+  }
 }
-
-export default App
