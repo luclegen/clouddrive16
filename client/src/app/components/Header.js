7d3b0c48f590fcc063b792e5e64a21e44348ac34
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-08 18:42:16 +0700

    Add Create search bar and avatar

--- client/src/app/components/Header.js
+++ client/src/app/components/Header.js
@@ -11,13 +11,26 @@ export default class Header extends Component {
 
   toggle = () => this.setState({isHover: !this.state.isHover})
 
+  logoWidth = () => document.querySelector('.logo').clientWidth
+
   render() {
     return (
       <header>
-        <nav>
+        <nav className="navbar">
           <a className="logo" href="/" onMouseEnter={this.toggle} onMouseLeave={this.toggle}>
-            <img src={'logo' + (this.state.isHover ? '.hover' : '') + '.png'} alt="Logo" />
+            <img className="logo-img" src={'logo' + (this.state.isHover ? '.hover' : '') + '.png'} alt="Logo" />
             {process.env.REACT_APP_CLIENT_NAME}
+          </a>
+          <div className="search-bar">
+            <form className="search-form">
+              <button className="search-btn" type="button">
+                <i className="material-icons">search</i>
+              </button>
+              <input className="search-in" type="search" placeholder="Search for anything" />
+            </form>
+          </div>
+          <a href="/" className="avatar">
+            <img className="avatar-img" src="https://lh3.googleusercontent.com/ogw/ADGmqu80fLiAIwlesuv_8mPJR4eMNwocFkqj4Cz8vcHj=s83-c-mo" alt="'s avatar" />
           </a>
         </nav>
       </header>
