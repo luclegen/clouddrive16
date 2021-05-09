6175aaee056379229dc3c24f0a61948f865e8040
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-09 23:25:03 +0700

    Update open, coloring, render and create reset

--- client/src/app/components/Header.js
+++ client/src/app/components/Header.js
@@ -10,7 +10,9 @@ export default class Header extends Component {
       isHover: false,
       selected: false,
       opened: false,
-      keyword: ''
+      keyword: '',
+      width: window.innerWidth,
+      e: null
     }
   }
 
@@ -21,63 +23,70 @@ export default class Header extends Component {
   getSearchBtn = () => document.querySelector('.search-btn')
 
   open = () => {
-    if (window.innerWidth < 801) {
+    if (this.state.width < 801) {
       this.getSearchBar().style.position = 'absolute'
       this.getSearchBar().style.width = '100%'
       this.getSearchBar().style.left = '0'
       this.getSearchBar().style.background = 'white'
       this.getSearchIn().style.display = 'block'
-      this.getSearchBtn().style.color = 'blue'
       this.getSearchIn().focus()
-      setTimeout(() => this.setState({opened: true}));
+      this.getSearchBtn().style.color = 'blue'
+      setTimeout(() => this.setState({opened: true}))
     }
   }
 
+  reset = (isMobile = true) => {
+    this.getSearchBar().style.width = isMobile ? 'auto' : (this.state.width < 1025 ? 250 : 500) + 'px'
+    this.getSearchBar().style.position = 'static'
+    this.getSearchBar().style.background = isMobile ? 'blue' : '#e1dfdd'
+    this.getSearchBtn().style.color = isMobile ? 'white' : 'blue'
+    this.getSearchIn().style.display = isMobile ? 'none' : 'flex'
+  }
+
   coloring = e => {
+    this.setState({e: e})
     if (e.type === 'select') this.setState({selected: true})
     if (e.type === 'blur') this.setState({selected: false})
-    
-    if (window.innerWidth > 800) {
+
+    if (this.state.width > 800) {
       let color = this.state.keyword || this.state.selected || e.type === 'mouseenter' ? 'white' : '#e1dfdd'
 
       if (e.type === 'select') color = 'white'
       if (e.type === 'blur' && !this.state.keyword) color = '#e1dfdd'
       document.querySelector('.search-bar').style.background = color
-    } else {
-      if (e.type === 'mouseleave') {
-        this.getSearchBar().style.width = 'auto'
-        this.getSearchBar().style.position = 'static'
-        this.getSearchBar().style.background = 'blue'
-        this.getSearchBtn().style.color = 'white'
-        this.getSearchIn().style.display = 'none'
-        this.setState({opened: false})
-      }
+    } else if (e.type === 'mouseleave') {
+      this.reset()
+      this.setState({opened: false})
     }
   }
 
   setKeyword = e => this.setState({keyword: e.target.value})
 
-  render() {
-    return (
-      <header>
-        <nav className="navbar">
-          <a className="logo" href="/" onMouseEnter={this.toggle} onMouseLeave={this.toggle}>
-            <img className="logo-img" src={'logo' + (this.state.isHover ? '.hover' : '') + '.png'} alt="Logo" />
-            {process.env.REACT_APP_CLIENT_NAME}
-          </a>
-          <div className="search-bar" onMouseEnter={this.coloring} onMouseLeave={this.coloring}>
-            <form className="search-form">
-              <button className="search-btn" type={window.innerWidth > 800 ? 'submit' : this.state.opened && this.state.keyword ? 'submit' : 'button'} disabled={window.innerWidth > 800 && !this.state.keyword} onClick={this.open}>
-                <i className="material-icons">search</i>
-              </button>
-              <input className="search-in" type="search" placeholder="Search for anything" onSelect={this.coloring} onBlur={this.coloring} onInput={this.setKeyword} />
-            </form>
-          </div>
-          <a href="/" className="avatar">
-            <img className="avatar-img" src="https://lh3.googleusercontent.com/ogw/ADGmqu80fLiAIwlesuv_8mPJR4eMNwocFkqj4Cz8vcHj=s83-c-mo" alt="'s avatar" />
-          </a>
-        </nav>
-      </header>
-    )
+  componentDidMount = () => window.onresize = () => {
+    this.setState({width: window.innerWidth})
+    if (this.state.e) this.reset(this.state.width < 801)
+    if (this.state.opened) this.open()
   }
+
+  render = () => (
+    <header>
+      <nav className="navbar">
+        <a className="logo" href="/" onMouseEnter={this.toggle} onMouseLeave={this.toggle}>
+          <img className="logo-img" src={'logo' + (this.state.isHover ? '.hover' : '') + '.png'} alt="Logo" />
+          {this.state.width > 560 ? process.env.REACT_APP_CLIENT_NAME : ''}
+        </a>
+        <div className="search-bar" onMouseEnter={this.coloring} onMouseLeave={this.coloring}>
+          <form className="search-form">
+            <button className="search-btn" type={this.state.width > 800 ? 'submit' : this.state.opened && this.state.keyword ? 'submit' : 'button'} disabled={this.state.width > 800 && !this.state.keyword} onClick={this.open}>
+              <i className="material-icons">search</i>
+            </button>
+            <input className="search-in" type="search" placeholder="Search for anything" onSelect={this.coloring} onBlur={this.coloring} onInput={this.setKeyword} />
+          </form>
+        </div>
+        <a href="/" className="avatar">
+          <img className="avatar-img" src="https://lh3.googleusercontent.com/ogw/ADGmqu80fLiAIwlesuv_8mPJR4eMNwocFkqj4Cz8vcHj=s83-c-mo" alt="'s avatar" />
+        </a>
+      </nav>
+    </header>
+  )
 }
