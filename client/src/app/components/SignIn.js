0156371254319b9f719310b616429ce9714a07f0
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-10 21:29:20 +0700

    Create SignIn component

---
+++ client/src/app/components/SignIn.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  test = () => console.log('email')
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(document.querySelector('body').clientHeight)
+    console.log('submit')
+  }
+
+  render = () => <section className="sign-in-section">
+    <form className="sign-in-form" onSubmit={this.onSubmit}>
+      <h1>Sign in</h1>
+      <div className="email-form-group">
+        <input className="email-in" type="email" name="email" id="email-in" placeholder="Email" />
+        <button className="in-btn" type="button" onClick={this.test}>
+          <i className="material-icons">input</i>
+        </button>
+      </div>
+      <div className="password-form-group">
+        <input className="password-in" type="password" name="password" id="password-in" placeholder="Password" />
+        <button className="in-btn" type="submit">
+          <i className="material-icons">input</i>
+        </button>
+      </div>
+    </form>
+  </section>
+}
+
+export default SignIn;
