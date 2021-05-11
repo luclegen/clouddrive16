5751ac7842f6100d80ab9e63c68b3eb8944e7952
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-11 11:32:20 +0700

    Update App component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,6 +1,6 @@
 import './App.sass'
-import { Component } from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { Component } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
