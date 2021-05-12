13e468745f073a115189854bfb54e80220054823
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-12 11:29:31 +0700

    Create check, reset and update onSubmit

--- client/src/app/components/Login.js
+++ client/src/app/components/Login.js
@@ -11,7 +11,8 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
-      remembered: false
+      remembered: false,
+      visible: false
     }
   }
 
@@ -21,28 +22,42 @@ class Login extends Component {
 
   setRemembered = e => this.setState({ remembered: e.target.checked })
 
-  test = () => console.log('email')
+  check = e => {
+    if (this.state.email && (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13))) {
+      this.setState({ visible: true })
+      setTimeout(() => document.querySelector('.form-group-password').style.height = 39 + 'px')
+    }
+  }
+
+  reset = () => {
+    if (!this.state.email) {
+      setTimeout(() => { if (document.querySelector('.form-group-password')) document.querySelector('.form-group-password').style.height = 0 })
+      this.setState({ visible: false, password: '' })
+    }
+  }
 
   onSubmit = e => {
     e.preventDefault()
-    console.log(this.state)
+    if (this.state.email && this.state.password) console.log(this.state)
   }
 
   render = () => <section className="section-login">
     <form className="form-login" onSubmit={this.onSubmit}>
       <img className='logo-img' src="/logo.png" alt={process.env.REACT_APP_CLIENT_NAME + ' logo'} />
       <h1 className="h1-login">Sign in to {process.env.REACT_APP_CLIENT_NAME}</h1>
-      <div className="form-group-email">
-        <input className="input-email" type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.setEmail} />
-        <button className="btn-input" type="button" onClick={this.test}>
+      <div className={`form-group-email ${this.state.visible ? 'rounded-top' : 'rounded'}`}>
+        <input className="input-email" type="email" name="email" placeholder="Email" value={this.state.email} onInput={this.reset} onChange={this.setEmail} onKeyUp={this.check} />
+        <button className="btn-input" type="button" disabled={!this.state.email} onClick={this.check}>
           <i className="material-icons">input</i>
         </button>
       </div>
-      <div className="form-group-password">
-        <input className="input-password" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.setPassword} />
-        <button className="btn-input" type="submit">
-          <i className="material-icons">input</i>
-        </button>
+      <div className="form-group-container">
+        {this.state.visible && this.state.email && <div className="form-group-password">
+          <input className="input-password" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.setPassword} />
+          <button className="btn-input" type="submit" disabled={!this.state.password}>
+            <i className="material-icons">input</i>
+          </button>
+        </div>}
       </div>
       <div className="form-check">
         <input className="form-check-input input-remember-me" id="remember-me" type="checkbox" name="remember-me" value={this.state.remembered} onChange={this.setRemembered} />
