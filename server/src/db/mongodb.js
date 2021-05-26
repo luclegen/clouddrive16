f4e6b5f6edeb8f4c06f2297bee7840403295fc55
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-26 13:29:07 +0700

    Create MongoDB

---
+++ server/src/db/mongodb.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('error', err => console.error(err));
+mongoose.connection.once('open', () => console.log('Connected to Mongoose'));
