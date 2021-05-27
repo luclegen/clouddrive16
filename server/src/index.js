f4372d993c9b49a1bb8381240563273e1d3980e2
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-27 17:20:22 +0700

    Add auth router and Update Error handle

--- server/src/index.js
+++ server/src/index.js
@@ -17,8 +17,11 @@ app.use(require('passport').initialize())
 // Notification
 app.get('/', (req, res) => res.send(`Started ${process.env.SERVER_NAME} server is successfuly!`))
 
+// Routes
+app.use('/auth', require('./routes/auth.router'))
+
 // Error handle
-app.use((err, req, res, next) => res.status(442).send(err.name == 'ValidationError' ? { msg: Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(', ') + '!' } : { error: err, msg: 'Unknown error!' }))
+app.use((err, req, res, next) => res.status(442).send(err.name == 'ValidationError' ? { msg: Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(', ') + '.' } : { error: err, msg: 'Unknown error.' }))
 
 // Start server
 app.listen(process.env.PORT, () => console.log(`Server started at http://localhost:${process.env.PORT || 5000}`))
