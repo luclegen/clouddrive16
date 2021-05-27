f0418ba121c8d202651c650c9f99e6a3c8bd9d8b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-27 17:19:14 +0700

    Add user

--- server/src/db/mongodb.js
+++ server/src/db/mongodb.js
@@ -8,4 +8,6 @@ mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('error', err => console.error(err));
-mongoose.connection.once('open', () => console.log('Connected to Mongoose'));
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
+
+require('../models/user.model');
