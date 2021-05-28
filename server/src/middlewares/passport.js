b3f14a52ccb45cd895c61e30c415d64ade933739
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-28 18:10:50 +0700

    Create passport

---
+++ server/src/middlewares/passport.js
@@ -0,0 +1,12 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user.model')
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: false
+}, (email, password, done) => User.findOne({ email: email })
+  .then(user => user ? user.verified(password) ? done(null, user) : done(null, false, { msg: 'Wrong password.' }) : done(null, false, { msg: 'Username not registered.' }))
+  .catch(err => done(err, false, { msg: 'Username not registered.' }))
+))
