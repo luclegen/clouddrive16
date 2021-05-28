48af57d12d2db4bc2bee6b4160859076b608d176
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-28 10:56:38 +0700

    Add checkEmail

--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const authCtl = require('../controllers/auth.controller')
 
+router.post('/check-email', authCtl.checkEmail)
 router.post('/register', authCtl.register)
 
 module.exports = router
