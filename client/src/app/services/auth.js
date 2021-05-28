ca2651aa824ddcb0f3a639a25a384ca15ae5d30c
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-28 18:27:03 +0700

    Create authenticate

--- client/src/app/services/auth.js
+++ client/src/app/services/auth.js
@@ -4,6 +4,8 @@ class AuthService {
   checkEmail = email => axios.post('auth/check-email', { email: email })
 
   register = user => axios.post('auth/register', user)
+
+  authenticate = user => axios.post('auth/authenticate', user)
 }
 
 export default new AuthService()
