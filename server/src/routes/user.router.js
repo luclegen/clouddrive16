93c6189b74dba46b2ca1d4f9f0b5ed906bf0e789
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-31 15:24:59 +0700

    Add read

--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
+const jwt = require('../middlewares/jwt');
 const userCtl = require('../controllers/user.controller')
 
 router.post('/', userCtl.create)
+router.get('/', jwt.verify, userCtl.read)
 
 module.exports = router
