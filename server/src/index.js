4897293d228722e366228370d766d07e697e330e
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-31 11:43:05 +0700

    Update ValidationError and Add TokenExpiredError

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,26 @@ app.use('/auth', require('./routes/auth.router'))
 app.use('/user', require('./routes/user.router'))
 
 // Error handle
-app.use((err, req, res, next) => res.status(442).send(err.name == 'ValidationError' ? { msg: Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(', ') + '.' } : { error: err, msg: 'Unknown error.' }))
+app.use((err, req, res, next) => {
+  let code = 520, msg = 'Unknown error.'
+
+  switch (err.name) {
+    case 'ValidationError':
+      code = 400
+      msg = Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(', ') + '.'
+      break
+
+    case 'TokenExpiredError':
+      code = 440
+      msg = 'Token has expired.'
+      break
+
+    default:
+      return res.status(code).send({ error: err, msg: msg })
+  }
+
+  return res.status(code).send({ msg: msg })
+})
 
 // Start server
 app.listen(process.env.PORT, () => console.log(`Server started at http://localhost:${process.env.PORT || 5000}`))
