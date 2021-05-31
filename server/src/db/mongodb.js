f4ad11d42e535e33c8ab22415d1955542fd8c1c4
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-31 09:32:09 +0700

    Update Create MongoDB

--- server/src/db/mongodb.js
+++ server/src/db/mongodb.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
 
-mongoose.connection.on('error', err => console.error(err));
-mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
+mongoose.connection.on('error', err => console.error(err))
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'))
 
-require('../models/user.model');
+require('../models/user.model')
