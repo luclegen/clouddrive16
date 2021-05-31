c0b0d8113b22fefbbaa72d209ceb85d538661a9d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-31 18:05:11 +0700

    Create private api

---
+++ client/src/app/api/private.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import helper from '../services/helper'
+
+const API = axios.create()
+
+API.interceptors.request.use(
+  async config => {
+    config.headers = {
+      'Authorization': `Bearer ${helper.getToken()}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    return config
+  },
+  err => Promise.reject(err)
+)
+
+API.interceptors.response.use(res => res, async err => {
+  if (err.response.status === 440 && window.confirm(err.response.data.msg)) {
+    localStorage.clear()
+    if (window.location.href !== process.env.REACT_APP_CLIENT_URL) window.open('/')
+    else window.location.reload()
+  } else alert(err.response.data.msg)
+  return Promise.reject(err)
+})
+
+export default API
