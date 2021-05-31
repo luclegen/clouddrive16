629d5896d7d38ae266fa23d6851aa179ef350609
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-31 09:47:21 +0700

    Create create

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,19 @@
+const User = require('../models/user.model')
+const checker = require('../helpers/checker')
+
+module.exports.create = (req, res, next) => {
+  if (!checker.isDate(req.body.year, req.body.month, req.body.day)) return res.status(403).send({ msg: 'Invalid date of birth.' })
+
+  const user = new User()
+
+  user.name.first = req.body.firstName
+  user.name.last = req.body.lastName
+  user.email = req.body.email
+  user.password = req.body.password
+  user.dateOfBirth = new Date(req.body.year, req.body.month, req.body.day)
+  user.sex = req.body.sex
+
+  user.save()
+    .then(() => res.status(201).send({ msg: 'Registered successfully.' }))
+    .catch(e => e.code === 11000 ? res.status(422).send({ msg: 'Email is duplicate. Please try again.' }) : next(e))
+}
