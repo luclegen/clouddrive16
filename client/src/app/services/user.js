9b95e6c2c280de707b9f0cb34215ed260eb830c2
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-05-31 18:07:04 +0700

    Create read

--- client/src/app/services/user.js
+++ client/src/app/services/user.js
@@ -1,9 +1,12 @@
 import PU from '../api/public'
+import PR from '../api/private'
 
 const URL = 'user/'
 
 class UserService {
   create = user => PU.post(URL, user)
+
+  read = () => PR.get(URL)
 }
 
 export default new UserService()
