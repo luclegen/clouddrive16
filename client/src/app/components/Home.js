8ba4a2fccf5cfaacd4cef82a44291b7417b69003
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-01 18:10:41 +0700

    Add Activate

--- client/src/app/components/Home.js
+++ client/src/app/components/Home.js
@@ -1,10 +1,11 @@
 import { Component } from 'react'
 import Login from './Login'
+import Activate from './Activate'
 import helper from '../services/helper'
 
 export default class Home extends Component {
 
   render = () => <main>
-    {!helper.loggedIn() && <Login />}
+    {helper.loggedIn() ? helper.getPayload().activated ? <section>Home</section> : <Activate /> : <Login />}
   </main>
 }
