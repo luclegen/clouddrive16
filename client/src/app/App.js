93d4c607578f56c523f40d8a4d7484ba5be972f9
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-02 13:01:56 +0700

    Update Header

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,7 +1,7 @@
 import './App.sass'
 import { Component } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import Header from './components/layouts/Header'
+import Header from './components/header/Header'
 import Home from './components/home/Home'
 
 export default class App extends Component {
