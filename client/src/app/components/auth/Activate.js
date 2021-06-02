f38681eb0279cc88a2c52526bacd59ff96c229a4
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-02 09:43:07 +0700

    Create getIndex, getInput and update Activate

--- client/src/app/components/auth/Activate.js
+++ client/src/app/components/auth/Activate.js
@@ -10,10 +10,12 @@ export default class Activate extends Component {
     }
   }
 
+  getIndex = target => Array.from(document.querySelector('.form-row-number').children).findIndex(i => i === target)
+
+  getInput = () => document.querySelectorAll('.form-control-digit')
+
   enterNumber = e => {
-    const ins = Array.from(document.querySelector('.form-row-number').children)
-    const i = ins.findIndex(i => i === e.target)
-    const input = document.querySelectorAll('.form-control-number')
+    const [i, input] = [this.getIndex(e.target), this.getInput()]
 
     if (e.target.value.length > 1) return input[i].value = e.target.value[0]
     if (i < 5 && helper.isDigit(input[i].value) && input[i].value.length === 1) input[i + 1].focus()
@@ -21,17 +23,13 @@ export default class Activate extends Component {
   }
 
   clearNumber = e => {
-    const ins = Array.from(document.querySelector('.form-row-number').children)
-    const i = ins.findIndex(i => i === e.target)
-    const input = document.querySelectorAll('.form-control-number')
+    const [i, input] = [this.getIndex(e.target), this.getInput()]
 
     if (e.keyCode === 8 && i > 0 && e.target.value === '') input[i - 1].focus()
   }
 
   pasteNumber = e => {
-    const ins = Array.from(document.querySelector('.form-row-number').children)
-    const i = ins.findIndex(i => i === e.target)
-    const input = document.querySelectorAll('.form-control-number')
+    const [i, input] = [this.getIndex(e.target), this.getInput()]
 
     const txt = (e.clipboardData || window.clipboardData).getData('text')
     const max = txt.length > 6 - i ? 6 - i : txt.length
@@ -47,7 +45,7 @@ export default class Activate extends Component {
     if (this.isSubmit()) this.onSubmit(e)
   }
 
-  isSubmit = () => '0'.repeat(6).split('').map((v, i) => v = document.querySelectorAll('.form-control-number')[i].value).filter(v => helper.isDigit(v)).length === 6
+  isSubmit = () => '0'.repeat(6).split('').map((v, i) => v = document.querySelectorAll('.form-control-digit')[i].value).filter(v => helper.isDigit(v)).length === 6
 
   onSubmit = e => {
     e.preventDefault()
@@ -60,7 +58,7 @@ export default class Activate extends Component {
       <img className='logo-img' src="/logo.png" alt={process.env.REACT_APP_CLIENT_NAME + ' logo'} />
       <h1 className="h1-only">Activate your account</h1>
       <div className="form-row-number">
-        {'0'.repeat(6).split('').map((v, i) => <input className="form-control-number" key={i} type="number" maxLength="1" onInput={this.enterNumber} onDrop={this.enterNumber} onKeyUp={this.clearNumber} onKeyDown={this.clearNumber} onPaste={this.pasteNumber} required />)}
+        {'0'.repeat(6).split('').map((v, i) => <input className="form-control-digit" key={i} type="number" maxLength="1" onInput={this.enterNumber} onDrop={this.enterNumber} onKeyUp={this.clearNumber} onKeyDown={this.clearNumber} onPaste={this.pasteNumber} required />)}
       </div>
     </form>
   </section>
