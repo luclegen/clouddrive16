71412137bbf8ce3e63ba1a0705f0d72bcc2ef3eb
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-02 14:16:01 +0700

    Update Activate and create, add send

--- client/src/app/components/auth/Activate.js
+++ client/src/app/components/auth/Activate.js
@@ -17,7 +17,7 @@ export default class Activate extends Component {
   select = e => e.target.select()
 
   enterNumber = e => {
-    const [i, input] = [this.getIndex(e.target), this.getInput()]
+    const [i, input] = [this.getIndex(e.target.closest('.form-group')), this.getInput()]
 
     if (e.target.value.length > 1) return input[i].value = e.target.value[0]
     if (i < 5 && helper.isDigit(input[i].value) && input[i].value.length === 1) input[i + 1].focus()
@@ -25,13 +25,13 @@ export default class Activate extends Component {
   }
 
   clearNumber = e => {
-    const [i, input] = [this.getIndex(e.target), this.getInput()]
+    const [i, input] = [this.getIndex(e.target.closest('.form-group')), this.getInput()]
 
     if (e.keyCode === 8 && i > 0 && e.target.value === '') input[i - 1].focus()
   }
 
   pasteNumber = e => {
-    const [i, input] = [this.getIndex(e.target), this.getInput()]
+    const [i, input] = [this.getIndex(e.target.closest('.form-group')), this.getInput()]
 
     const txt = (e.clipboardData || window.clipboardData).getData('text')
     const max = txt.length > 6 - i ? 6 - i : txt.length
@@ -47,6 +47,8 @@ export default class Activate extends Component {
     if (this.isSubmit()) this.onSubmit(e)
   }
 
+  send = e => e.preventDefault()
+
   isSubmit = () => '0'.repeat(6).split('').map((v, i) => v = document.querySelectorAll('.form-control-digit')[i].value).filter(v => helper.isDigit(v)).length === 6
 
   onSubmit = e => {
@@ -60,8 +62,11 @@ export default class Activate extends Component {
       <img className='logo-img' src="/logo.png" alt={process.env.REACT_APP_CLIENT_NAME + ' logo'} />
       <h1 className="h1-only">Activate your account</h1>
       <div className="form-row">
-        {'0'.repeat(6).split('').map((v, i) => <input className="form-control-digit" key={i} type="number" maxLength="1" onClick={this.select} onInput={this.enterNumber} onDrop={this.enterNumber} onKeyUp={this.clearNumber} onKeyDown={this.clearNumber} onPaste={this.pasteNumber} required />)}
+        {'0'.repeat(6).split('').map((v, i) => <div className="form-group" key={i}>
+          <input className="form-control-digit" type="number" maxLength="1" onClick={this.select} onInput={this.enterNumber} onDrop={this.enterNumber} onKeyUp={this.clearNumber} onKeyDown={this.clearNumber} onPaste={this.pasteNumber} required />
+        </div>)}
       </div>
+      <a href="/" onClick={this.send}>Resend Code</a>
     </form>
   </section>
 }
