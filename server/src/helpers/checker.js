2e02621121d61d084d6e95a19d7555317b238c73
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-02 15:05:11 +0700

    Update all

--- server/src/helpers/checker.js
+++ server/src/helpers/checker.js
@@ -1,9 +1,11 @@
-module.exports.isFirstName = v => /^[A-Z]{1}[a-z]*$/.test(v)
+module.exports.isFirstName = value => /^[A-Z]{1}[a-z]*$/.test(value)
 
-module.exports.isLastName = v => /^[A-Z]{1}[a-z]*(?: [A-Z]{1}[a-z]*)*(?: [A-Z]{1}[a-z]*)?$/.test(v)
+module.exports.isLastName = value => /^[A-Z]{1}[a-z]*(?: [A-Z]{1}[a-z]*)*(?: [A-Z]{1}[a-z]*)?$/.test(value)
 
-module.exports.isEmail = v => /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(v)
+module.exports.isEmail = value => /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(value)
 
-module.exports.isStrongPassword = v => v.length >= 8 && /[a-z]/ig.test(v) && /\d/g.test(v) && /[.@#$%^&*(),.?":{}|<>]/g.test(v)
+module.exports.isStrongPassword = value => value.length >= 8 && /[a-z]/ig.test(value) && /\d/g.test(value) && /[.@#$%^&*(),.?":{}|<>]/g.test(value)
 
-module.exports.isDate = (y, m, d, date = new Date(parseInt(m) + 1 + '/' + d + '/' + y)) => date.getFullYear() === parseInt(y) && date.getMonth() === parseInt(m) && date.getDate() === parseInt(d)
+module.exports.isDate = (year, month, day, date = new Date(parseInt(month) + 1 + '/' + d + '/' + year)) => date.getFullYear() === parseInt(year) && date.getMonth() === parseInt(month) && date.getDate() === parseInt(day)
+
+module.exports.isCode = value => /^\d{6}$/.test(value)
