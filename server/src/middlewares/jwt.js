2338beaad1e5d78b0bd35d22de95aea4f206fa89
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-02 15:31:43 +0700

    Rename verify to private

--- server/src/middlewares/jwt.js
+++ server/src/middlewares/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-module.exports.verify = async (req, res, next) => {
+module.exports.private = async (req, res, next) => {
   if ('authorization' in req.headers) {
     const token = req.headers['authorization'].split(' ')[1] === 'null' ? null : req.headers['authorization'].split(' ')[1]
 
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -3,6 +3,6 @@ const jwt = require('../middlewares/jwt');
 const userCtl = require('../controllers/user.controller')
 
 router.post('/', userCtl.create)
-router.get('/', jwt.verify, userCtl.read)
+router.get('/', jwt.private, userCtl.read)
 
 module.exports = router
