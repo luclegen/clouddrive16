bd04b6ecdbb768b8f52a2a70b7950c6eef116063
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-03 14:05:25 +0700

    Rename checkEmail to available

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,9 +2,9 @@ const passport = require('passport')
 const checker = require('../helpers/checker')
 const User = require('../models/user.model')
 
-module.exports.checkEmail = async (req, res, next) =>
+module.exports.available = async (req, res, next) =>
   User.findOne({ email: req.params.email })
     .then(user => res.status(user ? 203 : 200).send({ available: Boolean(user) }))
-    .catch(err => console.warn(err))
+    .catch(err => next(err))
 
 module.exports.authenticate = (req, res) => passport.authenticate('local', (err, user, info) => err ? res.status(400).json(err) : user ? res.status(200).json({ token: user.getToken() }) : res.status(404).json(info))(req, res)
--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const authCtl = require('../controllers/auth.controller')
 
-router.get('/:email', authCtl.checkEmail)
+router.get('/:email', authCtl.available)
 router.post('/', authCtl.authenticate)
 
 module.exports = router
