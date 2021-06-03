0e14804fbd59ae9c72ae0baad393291bb975fcd4
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-03 15:34:13 +0700

    Add verify

--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const jwt = require('../middlewares/jwt');
 const authCtl = require('../controllers/auth.controller')
 
 router.get('/:email', authCtl.available)
 router.post('/', authCtl.authenticate)
+router.put('/', jwt.private, authCtl.verify)
 
 module.exports = router
