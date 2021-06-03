0e55f299ae546cab17329f26aaf6be696f0bc3f3
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-03 22:26:53 +0700

    Create verify

--- client/src/app/services/auth.js
+++ client/src/app/services/auth.js
@@ -1,4 +1,5 @@
 import PU from '../apis/public'
+import PR from '../apis/private'
 
 const URL = 'auth/'
 
@@ -6,6 +7,8 @@ class AuthService {
   available = email => PU.get(`${URL}${email}`)
 
   authenticate = user => PU.post(`${URL}/`, user)
+
+  verify = code => PR.put(URL, code)
 }
 
 export default new AuthService()
