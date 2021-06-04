25912209b9fee4ee9c5b657500d26bb809400f40
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-04 09:53:35 +0700

    Create interceptors response

--- client/src/app/apis/public.js
+++ client/src/app/apis/public.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
 
-export default axios.create({
+const API = axios.create({
   timeout: 3000,
   headers: {
     'Content-Type': 'application/json',
   }
 })
+
+API.interceptors.response.use(res => res, async err => {
+  alert(err.response ? err.response.data.msg : err)
+  return Promise.reject(err)
+})
+
+export default API
