ee4f981e50c611841b86e1047994701037fa4d5b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-22 10:42:00 +0000

    Add git commit command

--- cli.sh
+++ cli.sh
@@ -11,3 +11,6 @@ cd client; npm i -s node-sass react-router-dom axios bootstrap@4.6 reactstrap@8.
 # II. EXPRESS.JS SERVER
 clear; npm start
 cd server; npm i -s dotenv mongoose express bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer rimraf
+
+# III. GIT
+git commit -m ""; git push
