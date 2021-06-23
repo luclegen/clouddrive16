61c1b45051da781cbd3bbd95f7b149b9be6e648b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-23 07:12:22 +0000

    Update Files and create functions

--- client/src/app/components/home/Files.js
+++ client/src/app/components/home/Files.js
@@ -1,6 +1,56 @@
 import { Component } from 'react'
+import helper from '../../services/helper'
+import userService from '../../services/user'
 
-export default class Home extends Component {
+export default class Files extends Component {
+  constructor(props) {
+    super(props)
 
-  render = () => <section>Files</section>
+    this.state = {
+      fullName: ''
+    }
+  }
+
+  getURLSearchParams = () => new URLSearchParams(window.location.search)
+
+  setQueryStringParameter = (name, value) => {
+    const params = new URLSearchParams(window.location.search)
+    params.set(name, value)
+    window.history.replaceState({}, "", decodeURIComponent(`${window.location.pathname}?${params}`))
+    window.location.reload()
+  }
+
+  deleteQueryStringParameter = name => {
+    const params = new URLSearchParams(window.location.search)
+    params.delete(name)
+    window.history.replaceState({}, "", decodeURIComponent(`${window.location.pathname}?${params}`))
+    window.location.reload()
+  }
+
+  setFiles = () => this.deleteQueryStringParameter('location')
+
+  setTrash = () => this.setQueryStringParameter('location', 'trash')
+
+  componentDidMount = () => {
+    if (helper.loggedIn())
+      userService.read()
+        .then(res => this.setState(res.data.user))
+    if (!window.location.search) window.location.search = 'id=root'
+  }
+
+  render = () => <section className="section-files d-flex">
+    <nav className="left-nav col-2" id="leftNav">
+      <div className="top-left-nav">
+        <label htmlFor="leftNav"><strong>{this.state.fullName}</strong></label>
+      </div>
+      <ul className="list-group">
+        <li className={`list-group-item list-group-item-action ${!this.getURLSearchParams().get('location') && 'active'}`} onClick={this.setFiles}><i className="material-icons">folder</i> My files</li>
+        <li className={`list-group-item list-group-item-action ${this.getURLSearchParams().get('location') === 'trash' && 'active'}`} onClick={this.setTrash}><i className="material-icons">delete</i> Trash</li>
+      </ul>
+    </nav>
+    <div className="right-content col-10">
+      <div className="command-bar"></div>
+      <div className="content"></div>
+    </div>
+  </section>
 }
