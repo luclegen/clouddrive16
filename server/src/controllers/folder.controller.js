8d960c670b908f2f588735e97bf16f2c556e782a
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-23 17:15:51 +0700

    Update create

--- server/src/controllers/folder.controller.js
+++ server/src/controllers/folder.controller.js
@@ -4,8 +4,8 @@ module.exports.create = (req, res, next) => {
   const folder = new Folder()
 
   folder._userId = req._id
-  if (req.body.path) folder.path = req.body.path
-  if (req.body.name) folder.name = req.body.name
+  folder.path = req.body.path
+  folder.name = req.body.name
 
   folder.save()
     .then(() => res.status(201).send({ msg: 'Folder created.' }))
