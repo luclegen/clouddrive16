2f3e9743a7530ee30ade22b6a390df9ad9e16178
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-23 17:46:24 +0700

    Update creat

--- server/src/controllers/folder.controller.js
+++ server/src/controllers/folder.controller.js
@@ -1,6 +1,10 @@
 const Folder = require('../models/folder.model')
 
 module.exports.create = (req, res, next) => {
+  Folder.find({ name: req.body.name, path: req.body.path })
+    .then(folders => folders.length && res.status(422).send({ msg: 'You already have a folder in the current path. Please a different name.' }))
+    .catch(err => next(err))
+
   const folder = new Folder()
 
   folder._userId = req._id
