3136aa0bc3dd2bee654cd6c2c151add5e79d1aa6
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-23 01:39:08 +0000

    Update cd

--- cli.sh
+++ cli.sh
@@ -4,10 +4,10 @@
 sudo npm i -g npm nodemon
 
 # I. REACT CLIENT
-clear; npm start
+cd client; clear; npm start
 npx create-react-app client
 cd client; npm i -s node-sass react-router-dom axios bootstrap@4.6 reactstrap@8.9 --legacy-peer-deps
 
 # II. EXPRESS.JS SERVER
-clear; npm start
+cd server; clear; npm start
 cd server; npm i -s dotenv mongoose express bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer rimraf
