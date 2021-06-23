ee720c2c372f94167a7ffd2c238ad383e30875b4
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-23 17:54:22 +0700

    Update create

--- server/src/controllers/folder.controller.js
+++ server/src/controllers/folder.controller.js
@@ -2,16 +2,21 @@ const Folder = require('../models/folder.model')
 
 module.exports.create = (req, res, next) => {
   Folder.find({ name: req.body.name, path: req.body.path })
-    .then(folders => folders.length && res.status(422).send({ msg: 'You already have a folder in the current path. Please a different name.' }))
+    .then(folders => {
+      if (folders.length) res.status(422).send({ msg: 'You already have a folder in the current path. Please a different name.' })
+      else {
+        const folder = new Folder()
+
+        folder._userId = req._id
+        folder.path = req.body.path
+        folder.name = req.body.name
+
+        folder.save()
+          .then(() => res.status(201).send({ msg: 'Folder created.' }))
+          .catch(err => next(err))
+      }
+    })
     .catch(err => next(err))
 
-  const folder = new Folder()
 
-  folder._userId = req._id
-  folder.path = req.body.path
-  folder.name = req.body.name
-
-  folder.save()
-    .then(() => res.status(201).send({ msg: 'Folder created.' }))
-    .catch(err => next(err))
 }
