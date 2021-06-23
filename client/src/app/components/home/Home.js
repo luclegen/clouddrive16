15199bef1c6bd98739b3946fcc7ffa7a6c5592d2
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-23 02:40:35 +0000

    Add Files

--- client/src/app/components/home/Home.js
+++ client/src/app/components/home/Home.js
@@ -1,11 +1,12 @@
 import { Component } from 'react'
 import Login from '../auth/Login'
 import Activate from '../auth/Activate'
+import Files from './Files'
 import helper from '../../services/helper'
 
 export default class Home extends Component {
 
   render = () => <main>
-    {helper.loggedIn() ? helper.getPayload().activated ? <section>Home</section> : <Activate /> : <Login />}
+    {helper.loggedIn() ? helper.getPayload().activated ? <Files /> : <Activate /> : <Login />}
   </main>
 }
