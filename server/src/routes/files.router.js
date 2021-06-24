1d057c34b92a8e0f3f1c22e66765ca9c7798f861
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-24 23:02:48 +0700

    Update files

--- server/src/routes/files.router.js
+++ server/src/routes/files.router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const jwt = require('../middlewares/jwt');
+const transfer = require('../helpers/transfer')
 const filesCtl = require('../controllers/files.controller')
 
+router.post('/', jwt.private, transfer.upload(process.env.FILES, 'files').array('files'), filesCtl.create)
 router.get('/', jwt.private, filesCtl.read)
 
 module.exports = router
