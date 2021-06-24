919466659d10de27178c644ed615eb8b29413a19
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-24 23:07:50 +0700

    Create create

--- client/src/app/services/files.js
+++ client/src/app/services/files.js
@@ -3,6 +3,8 @@ import PR from '../apis/private'
 const URL = `${process.env.REACT_APP_SERVER_URL}files/`
 
 class UserService {
+  create = files => PR.post(URL, files)
+
   read = () => PR.get(URL)
 }
 
