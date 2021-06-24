32fa0daf4167dd2813332ad9292d24d63e698d05
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-24 16:34:05 +0700

    Create and add getQueryStringParameter, open, access

--- client/src/app/components/home/Files.js
+++ client/src/app/components/home/Files.js
@@ -14,11 +14,15 @@ export default class Files extends Component {
       opened: false,
       path: '/',
       name: '',
-      folders: []
+      folder: { _id: 'root', path: '/', name: 'root' },
+      folders: [],
+      items: []
     }
   }
 
-  getURLSearchParams = () => new URLSearchParams(window.location.search)
+  getURLSearchParams = () => (new URLSearchParams(window.location.search))
+
+  getQueryStringParameter = (name) => (new URLSearchParams(window.location.search)).get(name)
 
   setQueryStringParameter = (name, value) => {
     const params = new URLSearchParams(window.location.search)
@@ -51,6 +55,20 @@ export default class Files extends Component {
     folderService.create({ name: name, path: this.state.path })
   }
 
+  open = e => {
+    const folder = this.state.folders.find(f => f._id === e.target.closest('.li-folder').id)
+    this.setState({ path: (this.state.path === '/' ? this.state.path : this.state.path + '/') + folder.name })
+    this.setQueryStringParameter('id', folder._id)
+  }
+
+  access = e => {
+    const index = Number.parseInt(e.target.id)
+    const path = index === 1 ? '/' : this.state.path.split('/').slice(0, index).join('/')
+    const folder = this.state.folders.find(f => f.path === path)
+    this.setState({ path: path ? path : '/' })
+    this.setQueryStringParameter('id', Number.parseInt(e.target.id) === 0 ? 'root' : folder._id)
+  }
+
   componentDidMount = () => {
     if (helper.loggedIn()) {
       userService.read()
@@ -59,9 +77,14 @@ export default class Files extends Component {
     if (!window.location.search) window.location.search = 'id=root'
   }
 
-  componentDidUpdate = () =>
+  componentDidUpdate = () => {
     foldersService.read()
-      .then(res => this.setState({ folders: res.data.folders }))
+      .then(res => {
+        const folder = this.getQueryStringParameter('id') === 'root' ? { path: '/', name: '' } : res.data.folders.find(f => f._id === this.getQueryStringParameter('id'))
+        const path = folder.name === '' ? '/' : folder.path === '/' ? folder.path + folder.name : folder.path + '/' + folder.name
+        this.setState({ folders: res.data.folders, items: res.data.folders.filter(f => f.path === path), path: path })
+      })
+  }
 
   render = () => <section className="section-files">
     <nav className="left-nav col-2" id="leftNav">
@@ -78,16 +101,16 @@ export default class Files extends Component {
         <button className="btn-new-folder" onClick={this.setFolder}><i className="material-icons">create_new_folder</i> New</button>
       </div>
       <div className="path-bar">
-        <strong>{`My files${this.state.path === '/' ? '' : this.state.path}`}</strong>
+        {this.state.path === '/' ? <strong>My files</strong> : this.state.path.split('/').map((v, i, a) => <div key={i}>{i === 0 ? <div className="dir"><p className="dir-parent" id={i} onClick={this.access}>My files</p><p>&nbsp;&gt;&nbsp;</p></div> : i === a.length - 1 ? <p><strong>{v}</strong></p> : <div className="dir"><p className="dir-parent" id={i} onClick={this.access}>{v}</p><p>&nbsp;&gt;&nbsp;</p></div>}</div>)}
       </div>
       <ul className="ls-folder">
-        {this.state.folders.map((v, i) => <li className="li-folder" key={i} id={`folder${i}`}>
+        {this.state.items.map((v, i, a) => a.length ? <li className="li-folder" key={i} id={v._id} onClick={this.open}>
           <img className="bg-folder" src="svg/lg-bg.svg" alt="background folder" />
           <div className="pad-folder"></div>
           {/* <img className="fg-folder" src="svg/lg-fg-media.svg" alt="forceground folder" /> */}
           <img className="fg-folder" src="svg/lg-fg.svg" alt="forceground folder" />
           <label className="label-folder" htmlFor={`folder${i}`}>{v.name}</label>
-        </li>)}
+        </li> : <li>This folder is empty</li>)}
       </ul>
     </div>
   </section>
