324d7cd62375c7896929ee0f2e5044b5bd179ee8
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-24 23:02:22 +0700

    Update files

--- server/src/controllers/files.controller.js
+++ server/src/controllers/files.controller.js
@@ -1,5 +1,25 @@
 const File = require('../models/file.model')
 
+module.exports.create = (req, res, next) =>
+  File.find({ name: req.body.name, path: req.body.path })
+    .then(files => {
+      if (files.length) res.status(422).send({ msg: 'File is duplicate. Please choose another file.' })
+      else {
+        JSON.parse(req.body.names).forEach(name => {
+          const file = new File()
+
+          file._userId = req._id
+          file.path = req.body.path
+          file.name = name
+
+          file.save()
+            .then(() => res.status(201).send({ msg: 'File created.' }))
+            .catch(err => next(err))
+        })
+      }
+    })
+    .catch(err => next(err))
+
 module.exports.read = (req, res, next) =>
   File.find({ _userId: req._id })
     .then(files => res.status(201).send({ files: files }))
