f14f353e12af8b0dc9f91bf85273d4ff117701f8
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-24 22:59:35 +0700

    Update upload and getImages

--- server/src/helpers/transfer.js
+++ server/src/helpers/transfer.js
@@ -1,35 +1,30 @@
-const multer = require('multer');
-const fs = require("fs");
+const multer = require('multer')
+const fs = require("fs")
 
 module.exports.getImages = (imgDir, callback) => {
-  let files = [];
+  let files = []
   fs.readdir(imgDir, function (err, ls) {
-    for (let i = 0; i < ls.length; i++) files.push(ls[i]);
-    callback(err, files);
+    for (let i = 0; i < ls.length; i++) files.push(ls[i])
+    callback(err, files)
   });
 }
 
 module.exports.upload = (root, dir = '', parentdir = '') => {
   const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-      const path = [];
-
-      path.push(root + '/' + (req.params.id ? req.params.id : ''));
-      if (dir) path.push(path[0] + '/' + (parentdir ? parentdir : dir));
+      const path = [root];
 
       switch (dir) {
         case 'files':
-          path.push(path[1] + '/' + JSON.parse(req.body.slideshow).color.replace(/#/, ''));
+          path.push(path[0] + req.body.path)
           break;
       }
 
-      path.forEach(p => { if (!fs.existsSync(p)) fs.mkdirSync(p); });
+      path.forEach(p => !fs.existsSync(p) && fs.mkdirSync(p));
 
-      callback(null, path[path.length - 1]);
+      callback(null, path[path.length - 1])
     },
-    filename: (req, file, callback) => {
-      callback(null, `${file.originalname}`);
-    }
+    filename: (req, file, callback) => callback(null, `${file.originalname}`)
   });
 
   return multer({ storage: storage });
