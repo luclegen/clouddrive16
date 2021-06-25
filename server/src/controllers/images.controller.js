5b937d5a59cad40ab3d29b7f7eaaf1c03eafd8a8
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-25 16:09:44 +0700

    Create read

---
+++ server/src/controllers/images.controller.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const url = require('url')
+const transfer = require('../helpers/transfer')
+
+module.exports.read = (req, res, next) => {
+  const img = url.parse(req.url, true).query.image
+  const dir = process.env.UPDATES
+
+  if (typeof img === 'undefined') {
+    transfer.getImages(dir, function (err, files) {
+      res.writeHead(200, { 'Content-type': 'text/html' })
+      res.end()
+    })
+  } else {
+    fs.readFile(dir + img, function (err, content) {
+      if (err) {
+        res.writeHead(400, { 'Content-type': 'text/html' })
+        res.end('No such image')
+      } else {
+        res.writeHead(200, { 'Content-type': 'image' })
+        res.end(content)
+      }
+    })
+  }
+}
