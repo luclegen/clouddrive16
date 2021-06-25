0a4854a2b8dfc4875612dd066c429e891da08406
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-25 21:55:05 +0700

    Add read

--- server/src/routes/file.router.js
+++ server/src/routes/file.router.js
@@ -4,5 +4,6 @@ const fileCtl = require('../controllers/file.controller')
 const transfer = require('../helpers/transfer')
 
 router.post('/', jwt.private, transfer.upload('uploads/files').single('file'), fileCtl.create)
+router.get('/:id', fileCtl.read)
 
 module.exports = router
