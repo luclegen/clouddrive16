cd8a2817237344da383036a532e7589681f01a40
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-26 22:07:45 +0700

    Add kill port

--- cli.sh
+++ cli.sh
@@ -12,3 +12,6 @@ cd client; npm i -s node-sass react-router-dom axios bootstrap@4.6 reactstrap@8.
 # II. EXPRESS.JS SERVER
 cd server; clear; npm start
 cd server; npm i -s dotenv mongoose express bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer rimraf
+
+# III. PORTS
+kill -9 $(lsof -t -i:3000)
