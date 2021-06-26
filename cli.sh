6bca6712181328ad9ea84629a63a73b77d17bd6e
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-26 23:15:32 +0700

    Add paths

--- cli.sh
+++ cli.sh
@@ -5,12 +5,12 @@ sudo npm i -g npm nodemon
 npm i -s
 
 # I. REACT CLIENT
-cd client; clear; npm start
+cd /workspace/cloud-drive-16/client; clear; npm start
 npx create-react-app client
 cd client; npm i -s node-sass react-router-dom axios bootstrap@4.6 reactstrap@8.9 --legacy-peer-deps
 
 # II. EXPRESS.JS SERVER
-cd server; clear; npm start
+cd /workspace/cloud-drive-16/server; clear; npm start
 cd server; npm i -s dotenv mongoose express bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer rimraf
 
 # III. PORTS
