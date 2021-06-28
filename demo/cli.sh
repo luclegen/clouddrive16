dfbbd0ca2fd3fc80ce31f99743510bebb18e616e
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-28 17:23:47 +0700

    Add heroku and kill 5000

--- demo/cli.sh
+++ demo/cli.sh
@@ -8,10 +8,27 @@ npm i -s
 cd /workspace/cloud-drive-16/client; clear; npm start
 npx create-react-app client
 cd client; npm i -s node-sass react-router-dom axios bootstrap@4.6 reactstrap@8.9 --legacy-peer-deps
+heroku login
+heroku create clouddrive16
+git init
+heroku git:remote -a clouddrive16
+git add .
+git commit -am "Create client"
+git branch -m main
+git push heroku main
 
 # II. EXPRESS.JS SERVER
 cd /workspace/cloud-drive-16/server; clear; npm start
 cd server; npm i -s dotenv mongoose express bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer rimraf
+heroku login
+heroku create clouddrive16-server
+git init
+heroku git:remote -a clouddrive16-server
+git add .
+git commit -am "Create server"
+git branch -m main
+git push heroku main
 
 # III. PORTS
 kill -9 $(lsof -t -i:3000)
+kill -9 $(lsof -t -i:5000)
