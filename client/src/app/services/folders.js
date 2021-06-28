1db37eb3b6544b33e25ea0a8831327f3bb215b0c
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-06-28 09:54:17 +0700

    Rename FoldersService

--- client/src/app/services/folders.js
+++ client/src/app/services/folders.js
@@ -2,8 +2,8 @@ import PR from '../apis/private'
 
 const URL = `${process.env.REACT_APP_SERVER_URL}folders/`
 
-class UserService {
+class FoldersService {
   read = () => PR.get(URL)
 }
 
-export default new UserService()
+export default new FoldersService()
