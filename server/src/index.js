d3dd0489590f5924183d867eee279f03b4b67e6f
Luc Huynh Tan <luc.dev.sun@gmail.com>
2021-11-11 16:07:37 +0700

    Update GET /

--- server/src/index.js
+++ server/src/index.js
@@ -15,8 +15,8 @@ app.use(express.json())
 app.use(require('passport').initialize())
 require('./middlewares/passport')
 
-// Notification
-app.get('/', (req, res) => res.send(`Started ${process.env.SERVER_NAME} server is successfully!`))
+// Access server and redirect to client
+app.get('/', (req, res) => res.redirect(`${process.env.CLIENT_URL}`))
 
 // Routes
 app.use('/auth', require('./routes/auth.router'))
