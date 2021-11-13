f97e8540b4dd4f9fe357489e87c84e4c1019282d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-11-13 10:05:32 +0700

    Add OAuth2

--- server/src/helpers/mailer.js
+++ server/src/helpers/mailer.js
@@ -1,25 +1,48 @@
 const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const OAuth2 = google.auth.OAuth2
 
-const transport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.SERVER_EMAIL,
-    pass: process.env.SERVER_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
-  }
-})
+const getTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.OAUTH_CLIENT_ID,
+    process.env.OAUTH_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  )
 
-module.exports.sendCode = (email, title, code) => {
+  oauth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN })
+
+  const accessToken = await new Promise((resolve, reject) => oauth2Client.getAccessToken((err, token) => {
+    if (err) reject("Failed to create access token :( " + err)
+    resolve(token)
+  }))
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.SERVER_EMAIL,
+      accessToken,
+      clientId: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  })
+
+  return transporter
+}
+
+module.exports.sendCode = async (email, title, code) => {
   const options = {
     from: `"${process.env.REACT_APP_NAME}" <${process.env.SERVER_EMAIL}>`,
     to: email,
     subject: title,
     text: '',
-    html: `<h1><img src="${process.env.SERVER_LOGO}" alt="${process.env.REACT_APP_NAME} Logo"> ${process.env.REACT_APP_NAME}</h1><hr><h1>` + title + '</h1><p>Verification code: ' + code + '</p>'
+    html: `<h1><img src="${process.env.EMAIL_LOGO}" alt="${process.env.REACT_APP_NAME} Logo"> ${process.env.REACT_APP_NAME}</h1><hr><h1>` + title + '</h1><p>Verification code: ' + code + '</p>'
   }
 
-  transport.sendMail(options)
-    .catch(err => console.warn(err))
+  try {
+    (await getTransporter()).sendMail(options, (err, info) => console.log(err ? err : 'Sent email: ' + info.response))
+  } catch (error) {
+    return console.log(error)
+  }
 }
