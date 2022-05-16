0f16bee8d13ccadbf317f3ce88d4533a49e8317d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-16 16:01:54 +0700

    Change app to api and web, add express-session

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,14 @@
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
+const sess = {
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {},
+}
+
+api.get('env') === 'production' && api.set('trust proxy', 1)
 
 // Environment Variables
 if (!process.env.NODE_ENV) require('dotenv').config()
@@ -8,34 +17,40 @@ if (!process.env.NODE_ENV) require('dotenv').config()
 require('./db/mongodb')
 
 // Initialization
-const app = express()
+const api = express()
+const web = express()
 const PORT = process.env.PORT || 5000
 
 // Middlewares
-if (process.env.NODE_ENV == 'production') app.use(express.static(path.resolve(__dirname, './views')))
-app.use(require('cors')())
-app.use(express.json())
-app.use(require('passport').initialize())
+if (process.env.NODE_ENV == 'production') web.use(express.static(path.resolve(__dirname, './views')))
+api.use(express.json())
+api.use(require('passport').initialize())
+api.use(session(sess))
+api.use(require('cors')({ origin: process.env.WEB, credentials: true }))
+api.use(require('cookie-parser')(process.env.SECRET))
 require('./middlewares/passport')
 
 // Routes
-app.use('/auth', require('./routes/auth.router'))
-app.use('/user', require('./routes/user.router'))
-app.use('/code', require('./routes/code.router'))
-app.use('/folder', require('./routes/folder.router'))
-app.use('/folders', require('./routes/folders.router'))
-app.use('/file', require('./routes/file.router'))
-app.use('/files', require('./routes/files.router'))
-app.use('/images', require('./routes/images.router'))
+api.use('/auth', require('./routes/auth.router'))
+api.use('/users', require('./routes/users.router'))
+api.use('/code', require('./routes/code.router'))
+api.use('/folder', require('./routes/folder.router'))
+api.use('/folders', require('./routes/folders.router'))
+api.use('/file', require('./routes/file.router'))
+api.use('/files', require('./routes/files.router'))
+api.use('/images', require('./routes/images.router'))
 
 // Client
 if (process.env.NODE_ENV == 'production')
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './views', 'index.html')))
+  web.get(['/find-account'], (req, res) => res.sendFile(path.join(__dirname, './views', "index.html")))
 else
-  app.get('/', (req, res) => res.send(`Started ${process.env.APP_NAME} server is successfully!`))
+  api.get('/', (req, res) => res.send(`Started ${process.env.NAME} server is successfully!`))
 
 // Error handle
-app.use(require('./middlewares/handler'))
+api.use(require('./middlewares/handler'))
+
+// Add api to web
+web.use('/api', api)
 
 // Start server
-app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
+web.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
