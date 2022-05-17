d5d7cca4b42fcf4a1fd0945acfce1734f6b56e24
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-17 16:16:25 +0700

    Change codeService to codesService, opened to isOpen

--- client/src/app/components/auth/Login.jsx
+++ client/src/app/components/auth/Login.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import helper from '../../services/helper'
 import Register from '../user/Register'
 import authService from '../../services/auth'
-import codeService from '../../services/code'
+import codesService from '../../services/codes'
 
 const state = {
   email: '',
@@ -10,40 +10,40 @@ const state = {
   remember: false,
   available: false,
   visible: false,
-  opened: false
+  isOpen: false
 }
 
 export default class Login extends Component {
   constructor(props) {
     super(props)
 
-    this.setEmail = this.setEmail.bind(this)
-    this.setPassword = this.setPassword.bind(this)
-    this.setRemember = this.setRemember.bind(this)
-
     this.state = state
   }
 
-  setEmail = e => this.setState({ email: e.target.value })
+  setValue = e => this.setState({ [e.target.getAttribute('name')]: e.target.value })
 
-  setPassword = e => this.setState({ password: e.target.value })
-
-  setRemember = e => this.setState({ remember: e.target.checked })
+  setChecked = e => this.setState({ [e.target.getAttribute('name')]: e.target.checked })
 
   enterEmail = async e => {
     if (e.target.value) {
-      const available = (await authService.available(e.target.value)).data.available
-      e.target.setCustomValidity(helper.isEmail(e.target.value) ? available ? '' : 'Email not registered' : 'Invalid email!')
+      const available = (await authService.available(e.target.value)).status === 200
+      e.target.setCustomValidity(helper.isEmail(e.target.value)
+        ? available
+          ? 'Email not registered'
+          : ''
+        : 'Invalid email!')
       this.setState({ available: available, visible: false, password: '' })
       setTimeout(() => {
-        if (document.querySelector('.form-group-password')) document.querySelector('.form-group-password').style.height = 0
+        if (document.querySelector('.form-group-password'))
+          document.querySelector('.form-group-password').style.height = 0
         document.querySelector('.input-email').style.width = 260 + 'px'
       })
     }
   }
 
-  open = () => this.setState({ opened: true })
-  close = () => this.setState({ opened: false })
+  open = () => this.setState({ isOpen: true })
+
+  close = () => this.setState({ isOpen: false })
 
   onSubmit = e => {
     e.preventDefault()
@@ -55,18 +55,15 @@ export default class Login extends Component {
         document.querySelector('.input-email').style.width = 315 + 'px'
         document.querySelector('.input-password').focus()
 
-        if (this.state.password)
-          authService.authenticate(this.state)
-            .then(res => {
-              this.state.remember ? localStorage.setItem('remember', this.state.remember) : sessionStorage.setItem('remember', this.state.remember)
-              helper.setToken(res.data.token)
-              this.setState(state)
-              if (!helper.getPayload().activated) {
-                alert('Your session exists for 5 minutes.')
-                codeService.create()
-              }
-              window.location.reload()
-            })
+        this.state.password && authService
+          .login(this.state)
+          .then(res => {
+            helper.setCookie(res.data, this.state.remember ? 365 * 24 * 60 * 60 : 0)
+            this.setState(state)
+            helper.getCookie('is_activate') === 'false'
+              && alert('Your session exists for 5 minutes.')
+            window.location.reload()
+          })
       })
     }
   }
@@ -78,26 +75,26 @@ export default class Login extends Component {
       <img className='logo-img' src="/logo.png" alt={process.env.REACT_APP_NAME + ' logo'} />
       <h1 className="h1-only">Sign in to {process.env.REACT_APP_NAME}</h1>
       <div className={`form-group-email ${this.state.visible ? 'rounded-top' : 'rounded'}`}>
-        <input className="input-email" type="email" name="email" placeholder="Email" value={this.state.email} pattern={helper.emailPattern} onInput={this.enterEmail} onInvalid={this.enterEmail} onChange={this.setEmail} title="Please fill out this field." required />
+        <input className="input-email" type="email" name="email" placeholder="Email" value={this.state.email} pattern={helper.emailPattern} onInput={this.enterEmail} onInvalid={this.enterEmail} onChange={this.setValue} title="Please fill out this field." required />
         {!this.state.visible && <button className="btn-input" type="submit" disabled={!this.state.email} hidden={true}>
           <i className="material-icons">input</i>
         </button>}
       </div>
       <div className="form-group-container">
         {this.state.visible && this.state.email && <div className="form-group-password">
-          <input className="input-password" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.setPassword} required />
+          <input className="input-password" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.setValue} required />
           <button className="btn-input" type="submit" disabled={!this.state.password}>
             <i className="material-icons">input</i>
           </button>
         </div>}
       </div>
       <div className="form-check-remember">
-        <input className="remember-me-input" id="remember-me" type="checkbox" name="remember-me" value={this.state.remember} onChange={this.setRemember} />
-        <label className="remember-me-label" htmlFor="remember-me">Keep me signed in</label>
+        <input className="remember-me-input" id="rememberLogin" type="checkbox" name="remember" value={this.state.remember} onChange={this.setChecked} />
+        <label className="remember-me-label" htmlFor="rememberLogin">Keep me signed in</label>
       </div>
       <a href="/find-account" target="_blank" rel="noopener noreferrer">Forgotten password? <i className="fas fa-external-link-alt"></i></a>
       <button className="btn-create-account" type="button" onClick={this.open}>Create New Account</button>
     </form>
-    {this.state.opened && <Register close={this.close}></Register>}
+    {this.state.isOpen && <Register close={this.close}></Register>}
   </section>
 }
