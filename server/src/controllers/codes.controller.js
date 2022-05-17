2e54fcf30bd0b5227bdbcb14b18732fa323dac3a
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-17 16:49:00 +0700

    Optimize create

--- server/src/controllers/codes.controller.js
+++ server/src/controllers/codes.controller.js
@@ -4,48 +4,42 @@ const _ = require('lodash')
 const Client = require('node-rest-client').Client
 const generator = require('../helpers/generator')
 
-module.exports.create = (req, res, next) => {
-  User.findById(req.payload)
-    .then(user => {
-      Code.findOne({ _uid: req.payload })
+module.exports.create = (req, res, next) => User.findById(req.payload)
+  .then(user => Code.findOne({ _uid: req.payload })
+    .then(code => {
+      const body = generator.genCode(6)
+
+      if (!code) {
+        code = new Code()
+        code._uid = user
+      }
+
+      code.body = body
+
+      code.save()
         .then(code => {
-          const body = generator.genCode(6)
+          if (user) {
+            if (code) {
+              const args = {
+                data: {
+                  email: user.email,
+                  title: 'Verify email',
+                  code: body
+                },
+                headers: {
+                  "Content-Type": "application/json",
+                  "Authorization": `Bearer ${process.env.TOKEN}`
+                }
+              }
+              const client = new Client()
 
-          if (!code) {
-            code = new Code()
-            code._uid = user
-          }
-
-          code.body = body
-
-          code.save()
-            .then(code => {
-              if (user) {
-                if (code) {
-                  const args = {
-                    data: {
-                      email: user.email,
-                      title: 'Verify email',
-                      code: body
-                    },
-                    headers: {
-                      "Content-Type": "application/json",
-                      "Authorization": `Bearer ${process.env.TOKEN}`
-                    }
-                  }
-
-                  const client = new Client()
-
-                  client.post(process.env.MAILER, args, (data, response) =>
-                    response.statusCode === 201
-                      ? res.status(201).send(data.toString(process.env.PLAIN))
-                      : res.stats(503).send('Mailer server not started or crashed!'))
-                } else res.status(404).send({ msg: 'Code not found.' })
-              } else res.status(404).send({ msg: 'User not found.' })
-            })
-            .catch(err => next(err))
+              client.post(process.env.MAILER, args, (data, response) =>
+                response.statusCode === 201
+                  ? res.status(201).send(data.toString(process.env.PLAIN))
+                  : res.stats(503).send('Mailer server not started or crashed!'))
+            } else res.status(404).send({ msg: 'Code not found.' })
+          } else res.status(404).send({ msg: 'User not found.' })
         })
-
-    })
-    .catch(err => next(err))
-}
+        .catch(err => next(err))
+    }))
+  .catch(err => next(err))
