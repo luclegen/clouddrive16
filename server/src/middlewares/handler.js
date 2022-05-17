2f39d3fb12d454064641942c8044e85f23df0d7f
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-17 09:14:07 +0700

    Add capitalize

--- server/src/middlewares/handler.js
+++ server/src/middlewares/handler.js
@@ -1,5 +1,10 @@
+const converter = require('../helpers/converter')
+
 module.exports = (err, req, res, next) => {
-  let code = 520, msg = 'Unknown error.'
+  let code = 500, msg = 'Server error!'
+
+  if (err.code === 11000)
+    return res.status(422).send(converter.capitalize(Object.keys(err.keyValue)[0] + ' is taken!'))
 
   switch (err.name) {
     case 'ValidationError':
@@ -7,19 +12,18 @@ module.exports = (err, req, res, next) => {
       msg = Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(';\n') + '.'
       break
 
-    case 'TokenExpiredError':
-      code = 401
-      msg = 'Login again?\nYour session has expired and must log in again.'
-      break
-
     case 'JsonWebTokenError':
       code = 400
       msg = err.message
       break
 
+    case 'TokenExpiredError':
+      code = 401
+      break
+
     default:
-      return res.status(code).send({ err: err, msg: msg })
+      return res.status(code).send(Object.entries(err).length ? err : converter.capitalize(msg))
   }
 
-  return res.status(code).send({ msg: msg })
+  return res.status(code).send(converter.capitalize(msg))
 }
