6ac4508a156e5d9b20f330ff92435e73495430c4
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-17 17:17:18 +0700

    Optimize authorize

--- server/src/middlewares/authorize.js
+++ server/src/middlewares/authorize.js
@@ -1,25 +1,19 @@
 const jwt = require('jsonwebtoken')
 
-module.exports = async (req, res, next) => {
-  const refuse = () =>
-    res
-      .clearCookie('connect.sid')
-      .clearCookie('id')
-      .clearCookie('avatar')
-      .clearCookie('firstName')
-      .clearCookie('lastName')
-      .sendStatus(401)
+module.exports = (req, res, next) => {
+  const refuse = () => res
+    .clearCookie('connect.sid')
+    .clearCookie('id')
+    .clearCookie('avatar')
+    .clearCookie('firstName')
+    .clearCookie('lastName')
+    .clearCookie('is_activate')
+    .sendStatus(401)
 
-  if (req.session?.token) {
-    try {
-      const payload = await jwt.verify(req.session?.token, process.env.SECRET)
-
-      if (payload) {
-        req.payload = payload
-        next()
-      } else return refuse()
-    } catch (err) {
-      next(err)
-    }
-  } else return refuse()
+  req.session?.token
+    ? jwt.verify(req.session?.token, process.env.SECRET, (err, payload) =>
+      err
+        ? next(err) || refuse()
+        : (req.payload = payload) && next())
+    : refuse()
 }
