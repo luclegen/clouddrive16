1a3e1f8ba63ffe7af27eb1637e8df96615a55cfe
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-17 17:28:26 +0700

    Remove PU, PR and add API

--- client/src/app/services/users.js
+++ client/src/app/services/users.js
@@ -1,13 +1,11 @@
 import API from '../apis/api'
-import PU from '../apis/public'
-import PR from '../apis/private'
 
 const URL = `${process.env.REACT_APP_API}/users/`
 
 class UsersService {
   create = user => API.post(URL, user)
 
-  read = () => PR.get(URL)
+  read = () => API.get(URL)
 }
 
 export default new UsersService()
