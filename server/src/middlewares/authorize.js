c7cf228babd03637acbe313c2f7ccfacebfbd43f
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-18 09:13:28 +0700

    Check TokenExpiredError

--- server/src/middlewares/authorize.js
+++ server/src/middlewares/authorize.js
@@ -3,16 +3,18 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => req.session?.token
   ? jwt.verify(req.session?.token, process.env.SECRET, (err, payload) =>
     err
-      ? res
-        .clearCookie('connect.sid')
-        .clearCookie('id')
-        .clearCookie('avatar')
-        .clearCookie('firstName')
-        .clearCookie('lastName')
-        .clearCookie('is_activate')
-        .status(401)
-        .send('Login session has expired!')
-      : (req.payload = payload) && next())
+      ? err.name === 'TokenExpiredError'
+        ? res
+          .clearCookie('connect.sid')
+          .clearCookie('id')
+          .clearCookie('avatar')
+          .clearCookie('firstName')
+          .clearCookie('lastName')
+          .clearCookie('is_activate')
+          .status(401)
+          .send('Login session has expired!')
+        : (req.payload = payload) && next()
+      : next(err))
   : res
     .clearCookie('connect.sid')
     .clearCookie('id')
