17994604b4bcb07fd792abccdbf50f38850928f2
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-19 09:07:19 +0700

    Add update

--- server/src/routes/folders.router.js
+++ server/src/routes/folders.router.js
@@ -4,6 +4,7 @@ const foldersController = require('../controllers/folders.controller')
 
 router.post('/', authorize, foldersController.create)
 router.get('/:id', authorize, foldersController.read)
+router.put('/:id', authorize, foldersController.update)
 router.get('/', authorize, foldersController.list)
 
 module.exports = router
