81b5dfcba428460685cb1e0d76108a2d03e39990
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-19 08:36:11 +0700

    Remove PR and change read to list

--- client/src/app/services/folders.js
+++ client/src/app/services/folders.js
@@ -1,12 +1,11 @@
 import API from '../apis/api'
-import PR from '../apis/private'
 
 const URL = `${process.env.REACT_APP_API}/folders/`
 
 class FoldersService {
   create = folder => API.post(URL, folder)
 
-  read = () => PR.get(URL)
+  list = () => API.get(URL)
 }
 
 export default new FoldersService()
