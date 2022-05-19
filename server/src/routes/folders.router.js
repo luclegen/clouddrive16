a06146327c8aa856a670401331661d345b73827b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-19 08:35:19 +0700

    Change read to list

--- server/src/routes/folders.router.js
+++ server/src/routes/folders.router.js
@@ -3,6 +3,6 @@ const authorize = require('../middlewares/authorize');
 const foldersController = require('../controllers/folders.controller')
 
 router.post('/', authorize, foldersController.create)
-router.get('/', authorize, foldersController.read)
+router.get('/', authorize, foldersController.list)
 
 module.exports = router
