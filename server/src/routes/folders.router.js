a116de27151cfb71cf4634501e7e3f6f5bd9fa7a
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-19 08:44:29 +0700

    Add read

--- server/src/routes/folders.router.js
+++ server/src/routes/folders.router.js
@@ -3,6 +3,7 @@ const authorize = require('../middlewares/authorize');
 const foldersController = require('../controllers/folders.controller')
 
 router.post('/', authorize, foldersController.create)
+router.get('/:id', authorize, foldersController.read)
 router.get('/', authorize, foldersController.list)
 
 module.exports = router
