b7708798de6b2da29ebe547cc6dd0ee129e0369d
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-22 14:20:14 +0700

    Remove root, ; and change originalName to originalname

--- server/src/helpers/transfer.js
+++ server/src/helpers/transfer.js
@@ -3,10 +3,10 @@ const fs = require("fs")
 
 module.exports.getImages = (dir, callback) => fs.readdir(dir, (err, files) => callback(err, files))
 
-module.exports.upload = (root, dir = '') => {
+module.exports.upload = (dir = '') => {
   const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-      const path = [process.env.UPLOADS];
+      const path = [process.env.UPLOADS]
 
       path[1] = path[0] + req.payload._id
 
@@ -14,15 +14,15 @@ module.exports.upload = (root, dir = '') => {
         case 'files':
           path[2] = path[1] + '/files'
           path[3] = path[2] + req.body.path
-          break;
+          break
       }
 
-      path.forEach(p => !fs.existsSync(p) && fs.mkdirSync(p));
+      path.forEach(p => !fs.existsSync(p) && fs.mkdirSync(p))
 
       callback(null, path[path.length - 1])
     },
-    filename: (req, file, callback) => callback(null, `${file.originalName}`)
-  });
+    filename: (req, file, callback) => callback(null, `${file.originalname}`)
+  })
 
-  return multer({ storage: storage });
+  return multer({ storage: storage })
 }
