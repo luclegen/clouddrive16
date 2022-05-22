0ce993df899774bf57e8231ba67bdaf23fe33297
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-22 17:01:57 +0700

    Add download

--- server/src/routes/files.router.js
+++ server/src/routes/files.router.js
@@ -4,6 +4,7 @@ const transfer = require('../helpers/transfer')
 const filesController = require('../controllers/files.controller')
 
 router.post('/', authorize, transfer.upload('files').array('files'), filesController.create)
+router.get('/d/:id', authorize, filesController.download)
 router.get('/', authorize, filesController.list)
 
 module.exports = router
