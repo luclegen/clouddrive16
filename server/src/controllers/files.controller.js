58346729a23705f8ac391bd87850b867b7990280
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-22 14:27:25 +0700

    switch JSON.parse and File.find

--- server/src/controllers/files.controller.js
+++ server/src/controllers/files.controller.js
@@ -1,11 +1,11 @@
 const File = require('../models/file.model')
 
 module.exports.create = (req, res, next) =>
-  File.find({ _uid: req.payload, name: req.body.name, path: req.body.path })
-    .then(files => {
-      if (files.length) res.status(422).send('File is duplicate. Please choose another file.')
-      else {
-        JSON.parse(req.body.names).forEach(name => {
+  JSON.parse(req.body.names).forEach(name =>
+    File.find({ _uid: req.payload, name: name, path: req.body.path })
+      .then(files => {
+        if (files.length) res.status(422).send('File is duplicate. Please choose another file.')
+        else {
           const file = new File()
 
           file._uid = req.payload
@@ -15,10 +15,9 @@ module.exports.create = (req, res, next) =>
           file.save()
             .then(() => res.status(201).send())
             .catch(err => next(err))
-        })
-      }
-    })
-    .catch(err => next(err))
+        }
+      })
+      .catch(err => next(err)))
 
 module.exports.list = (req, res, next) =>
   File.find({ _uid: req.payload })
