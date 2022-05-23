8fc08d83bf1a6313619364360e27bf3d483e150f
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-23 09:48:32 +0700

    Create read

--- server/src/controllers/files.controller.js
+++ server/src/controllers/files.controller.js
@@ -1,5 +1,5 @@
-var path = require('path')
 const File = require('../models/file.model')
+const _ = require('lodash')
 
 module.exports.create = (req, res, next) =>
   JSON.parse(req.body.names).forEach(name =>
@@ -23,6 +23,11 @@ module.exports.create = (req, res, next) =>
 module.exports.download = (req, res, next) =>
   File.findById(req.params.id)
     .then(file => res.download(process.env.UPLOADS + file._uid + '/files' + file.path + '/' + file.name))
+    .catch(err => next(err))
+
+module.exports.read = (req, res, next) =>
+  File.findById(req.params.id)
+    .then(file => res.send(_.pick(file, ['path', 'name', 'is_trash'])))
     .catch(err => next(err))
 
 module.exports.list = (req, res, next) =>
