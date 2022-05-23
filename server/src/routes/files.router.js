71dfd085ec3202a60280d24853c1fbb86c7bab34
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-23 10:06:31 +0700

    Add update

--- server/src/routes/files.router.js
+++ server/src/routes/files.router.js
@@ -6,6 +6,7 @@ const filesController = require('../controllers/files.controller')
 router.post('/', authorize, transfer.upload('files').array('files'), filesController.create)
 router.get('/d/:id', authorize, filesController.download)
 router.get('/:id', authorize, filesController.read)
+router.put('/:id', authorize, filesController.update)
 router.get('/', authorize, filesController.list)
 
 module.exports = router
