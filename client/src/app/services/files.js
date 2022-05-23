cfae9f68566efb7070d1814106c6f9ec89ed5550
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-05-23 09:56:10 +0700

    Create read

--- client/src/app/services/files.js
+++ client/src/app/services/files.js
@@ -5,6 +5,8 @@ const URL = `${process.env.REACT_APP_API}/files/`
 class FilesService {
   create = files => API.post(URL, files)
 
+  read = id => API.get(`${URL}${id}`)
+
   list = () => API.get(URL)
 }
 
