5947892cc3076978c15492814d41cd9e8d61b6eb
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-15 08:58:56 +0700

    Create toPath and toUploadPath

--- server/src/helpers/converter.js
+++ server/src/helpers/converter.js
@@ -1,1 +1,5 @@
 module.exports.capitalize = text => text.length > 0 ? text[0].toUpperCase() + text.slice(1) : text
+
+module.exports.toPath = value => value.path + (value.path === '/' ? '' : '/') + value.name
+
+module.exports.toUploadPath = (id, value) => process.env.UPLOADS + id + '/files' + this.toPath(value)
