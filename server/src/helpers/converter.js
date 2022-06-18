5b3b2d1f3a88efe38c597526b1300651a099836b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-18 16:47:46 +0700

    Create toFile, add copyFolderInFolder to toUploadPath

--- server/src/helpers/converter.js
+++ server/src/helpers/converter.js
@@ -1,5 +1,9 @@
+const duplicator = require('./duplicator')
+
 module.exports.capitalize = text => text.length > 0 ? text[0].toUpperCase() + text.slice(1) : text
 
 module.exports.toPath = value => value.path + (value.path === '/' ? '' : '/') + value.name
 
-module.exports.toUploadPath = (id, value) => process.env.UPLOADS + id + '/files' + this.toPath(value)
+module.exports.toUploadPath = (id, value, array) => process.env.UPLOADS + id + '/files' + (array?.length ? this.toPath(value, array) : this.toPath(value))
+
+module.exports.toFile = (filename, elements = filename.split(/\./)) => ({ name: filename.slice(0, filename.length - elements[elements.length - 1].length - 1), extension: filename.slice(filename.length - elements[elements.length - 1].length - 1) })
