b90a050eb3d4496ac88930479d2aabb1ebcdb4da
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-19 14:23:12 +0700

    Create toRegex

--- server/src/helpers/converter.js
+++ server/src/helpers/converter.js
@@ -7,3 +7,5 @@ module.exports.toPath = value => value.path + value.name + '/'
 module.exports.toUploadPath = (id, value, array) => process.env.UPLOADS + id + '/files' + (array?.length ? this.toPath(value, array) : this.toPath(value))
 
 module.exports.toFile = (filename, elements = filename.split(/\./)) => ({ name: filename.slice(0, filename.length - elements[elements.length - 1].length - 1), extension: filename.slice(filename.length - elements[elements.length - 1].length - 1) })
+
+module.exports.toRegex = path => path.replaceAll('(', '\\(').replaceAll(')', '\\)').replaceAll('[', '\\[').replaceAll(']', '\\]').replaceAll('{', '\\{').replaceAll('}', '\\}')
