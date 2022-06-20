287534e29b5067c69b1ae94bb10a60b4d0fbc9dc
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-20 15:10:36 +0700

    Add createRoot, Provider and store, container, root

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.sass'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 import App from './app/App'
 import reportWebVitals from './reportWebVitals'
+import './index.sass'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
