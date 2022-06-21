c80b554f5f365c448f4db24481258c030e376a64
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-06-21 14:09:54 +0700

    Revert index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
+import ReactDOM from 'react-dom'
+import './index.sass'
 import App from './app/App'
 import reportWebVitals from './reportWebVitals'
-import './index.sass'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 )
 
 reportWebVitals()
