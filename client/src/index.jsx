fcd42dbe0c2b192ec13b596a1002c2cbdc5fa3d0
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-07-04 13:52:48 +0700

    Add store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.sass'
-import App from './app/App'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './App/store'
+import App from './App'
 import reportWebVitals from './reportWebVitals'
+import './index.sass'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
 
 reportWebVitals()
