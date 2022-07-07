1ca3f44c1cefe4510b1d9435599e01057ff8db11
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-07-07 16:58:33 +0700

    Create formDataAPI

---
+++ client/src/App/apis/form-data.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const formDataAPI = axios.create({
+  withCredentials: true,
+  headers: {
+    "Content-Type": "multipart/form-data",
+    "Accept": "multipart/form-data",
+  },
+})
+
+formDataAPI.interceptors.response.use(res => res, err => {
+  err.response
+    ? alert(typeof err.response?.data === 'object'
+      ? JSON.stringify(err.response?.data)
+      : err.response?.data || err.response?.statusText)
+    || (err.response.status === 501 && window.location.reload())
+    : console.error(err)
+
+  return Promise.reject(err)
+})
+
+export default formDataAPI 
