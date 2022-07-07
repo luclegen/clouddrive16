af978d75c036ff40d7bce1f9e529eff7013168ed
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-07-07 14:59:51 +0700

    Create verify

--- client/src/App/components/Activate/slice.js
+++ client/src/App/components/Activate/slice.js
@@ -5,11 +5,9 @@ import authService from '../../services/auth'
 
 const initialState = {
   sent: false,
-  is_activate: helper.getCookie('is_activate') === 'true',
 }
 
 export const create = createAsyncThunk('activate/create', async () => (await codesService.create()).data)
-export const verify = createAsyncThunk('activate/verify', async code => (await authService.verify(code)).data)
 
 export const activateSlice = createSlice({
   name: 'activate',
@@ -20,18 +18,10 @@ export const activateSlice = createSlice({
       state.sent = true
       window.open(action.payload)
     })
-    .addCase(verify.fulfilled, state => {
-      state.is_activate = true
-    })
-    .addCase(verify.rejected, () => {
-      document.querySelector('.form-only').reset()
-      document.querySelector('.form-control-digit').focus()
-    })
 })
 
 export const { toggle, setOpened, toggleDropdown } = activateSlice.actions
 
 export const selectSent = state => state.activate.sent
-export const selectIsActivate = state => state.activate.is_activate
 
 export default activateSlice.reducer
