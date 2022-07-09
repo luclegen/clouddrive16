bfb77fce3a8796134f42765835341760403eab18
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-07-09 08:25:02 +0700

    Update upload and save, remove percent

--- client/src/App/components/Files/index.jsx
+++ client/src/App/components/Files/index.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Progress } from 'reactstrap'
+import Progress from '../Progress'
 import {
   createFolder,
   list,
@@ -20,6 +20,8 @@ import {
 import {
   setWidth,
 } from '../Header/slice'
+import { setValue, selectShowProgress, hideProgress, showProgressComponent, setUploadFiles, selectUploadFiles, selectControllers } from '../Progress/slice'
+import formDataAPI from '../../apis/form-data'
 import Media from '../Media'
 import FolderTree from '../FolderTree'
 import helper from '../../services/helper'
@@ -36,11 +38,13 @@ export default function Files() {
   const medias = useSelector(selectMedias)
   const media = useSelector(selectMedia)
   const index = useSelector(selectIndex)
+  const showProgress = useSelector(selectShowProgress)
+
+  const controllers = useRef(null)
 
   const [id, setID] = useState('')
   const [type, setType] = useState('')
-  const [percent, setPercent] = useState(0)
-  const [show, setShow] = useState(false)
+  const [showFolderTree, setShowFolderTree] = useState(false)
   const [name, setName] = useState('')
   const [parent, setParent] = useState('')
   const [action, setAction] = useState('')
@@ -66,6 +70,31 @@ export default function Files() {
   const create = () => dispatch(createFolder({ name: prompt('Create folder', 'New folder'), path: path }))
     .then(action => action.type === 'files/createFolder/fulfilled' && refresh())
 
+  const upload = () => document.getElementById("files").click()
+
+  const save = async e => {
+    const files = Array.from(e.target.files).map(v => ({ name: v.name, value: 0, show: true, cancel: false }))
+    controllers.current = '0'.repeat(files.length).split('').map(() => new AbortController())
+
+    await dispatch(setUploadFiles(files))
+    await dispatch(showProgressComponent())
+
+    controllers.current.length && Array.from(e.target.files)
+      .forEach((v, i) => {
+        const formData = new FormData()
+
+        formData.append("path", path)
+        formData.append("files", v, v.name)
+        formData.append("names", JSON.stringify([v.name]))
+
+        formDataAPI.post(`${process.env.NODE_ENV === 'production' ? window.location.origin + '/api' : process.env.REACT_APP_API}/files/`, formData, {
+          signal: controllers.current[i].signal,
+          onUploadProgress: data => dispatch(setValue({ index: i, value: Math.round(100 * (data.loaded / data.total)) })),
+        })
+          .then(() => refresh() && dispatch(setUploadFiles([])) && dispatch(hideProgress()))
+      })
+  }
+
   const move = () => { }
 
   const copy = () => { }
@@ -89,10 +118,6 @@ export default function Files() {
   const restoreTrash = () => { }
 
   const empty = () => { }
-
-  const upload = () => { }
-
-  const save = () => { }
 
   const open = e => {
     if ((/folder/g).test(e.target.className) || e.target.closest('.li-folder')) {
@@ -186,9 +211,8 @@ export default function Files() {
         : <span className="main-command-bar">
           <button className="btn-new-folder" onClick={create}><i className="material-icons">create_new_folder</i> New</button>
           <input type="file" id="files" onChange={save} multiple hidden />
-          <button className="btn-update" onClick={upload}><i className="material-icons">publish</i> Upload</button>
+          <button className="btn-upload" onClick={upload}><i className="material-icons">publish</i> Upload</button>
         </span>}
-      {!!percent && <Progress value={percent} />}
       {helper.getQuery('location') === 'trash'
         ? !isEmpty() && <div className="space-bar"></div>
         : helper.getQuery('id')
@@ -225,7 +249,8 @@ export default function Files() {
       </ul>}
       {isEmpty() && <div className="empty-trash"><i className="material-icons">delete_outline</i><strong>Trash is Empty</strong></div>}
     </main>
-    {media && <Media src={media} type={helper.isImage(media) ? 'image' : helper.isVideo(media) ? 'video' : helper.isAudio(media) ? 'audio' : 'none'} download={download} percent={percent} next={next} prev={prev} index={index} count={medias?.length} close={close} />}
-    {show && <FolderTree id={id} name={name} type={type} parent={parent} action={action} refresh={refresh} folders={folders} close={closeFolderTree} />}
+    {media && <Media src={media} type={helper.isImage(media) ? 'image' : helper.isVideo(media) ? 'video' : helper.isAudio(media) ? 'audio' : 'none'} download={download} next={next} prev={prev} index={index} count={medias?.length} close={close} />}
+    {showFolderTree && <FolderTree id={id} name={name} type={type} parent={parent} action={action} refresh={refresh} folders={folders} close={closeFolderTree} />}
+    {showProgress && <Progress controllers={controllers} />}
   </section>
 }
