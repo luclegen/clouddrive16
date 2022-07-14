97f6ef6d97708676e1ad40ec814b3daef2cb782e
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-07-14 17:29:38 +0700

    Add createPlaintext

--- server/src/routes/files.router.js
+++ server/src/routes/files.router.js
@@ -4,6 +4,7 @@ const transfer = require('../helpers/transfer')
 const filesController = require('../controllers/files.controller')
 
 router.post('/', authorize, transfer.upload('files').array('files'), filesController.create)
+router.post('/p', authorize, filesController.createPlaintext)
 router.get('/d/:id', authorize, filesController.download)
 router.get('/:id', authorize, filesController.read)
 router.put('/:id', authorize, filesController.update)
