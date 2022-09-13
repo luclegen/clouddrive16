87166e7c4ad6661efd01c445ebc1eef0b53b80c8
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2022-09-13 16:46:11 +0700

    Change imageController to mediaController

--- server/src/routes/media.router.js
+++ server/src/routes/media.router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authorize = require('../middlewares/authorize');
-const imageController = require('../controllers/media.controller');
+const mediaController = require('../controllers/media.controller');
 
-router.get('/', authorize, imageController.list);
+router.get('/', authorize, mediaController.list);
 
 module.exports = router;
