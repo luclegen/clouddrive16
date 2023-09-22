d804ecc0af905680475dd44b23e15d2792468579
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-09-22 08:17:14 +0700

    Add body-parser, swagger

--- demo/cli.sh
+++ demo/cli.sh
@@ -22,7 +22,7 @@ git push heroku main
 # II. EXPRESS.JS SERVER
 cd /workspace/cloud-drive-16/server; clear; npm start
 mkdir server; cd server; mkdir src; touch src/index.js; npm init -y
-cd server; npm i -s dotenv mongoose express bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer rimraf
+cd server; npm i -s dotenv mongoose express bcryptjs cors body-parser jsonwebtoken passport passport-local lodash swagger-ui-express swagger-jsdoc nodemailer rimraf
 heroku login
 heroku create clouddrive16-server
 xdg-open https://dashboard.heroku.com/apps/clouddrive16-server/settings
