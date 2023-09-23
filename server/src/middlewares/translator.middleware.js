5b50864907ff9f3adad0b6ddf97cf074009b871a
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-09-23 15:23:11 +0700

    Create translator middleware

---
+++ server/src/middlewares/translator.middleware.js
@@ -0,0 +1,11 @@
+const i18next = require('i18next')
+const middleware = require('i18next-http-middleware')
+const Lang = require('../models/lang.enum')
+
+i18next.use(require('i18next-fs-backend')).use(middleware.LanguageDetector).init({
+  preload: Object.values(Lang),
+  fallbackLng: Lang.EN,
+  backend: { loadPath: './locales/{{lng}}.json' }
+})
+
+module.exports = middleware.handle(i18next, { ignoreRoutes: [] })
