45e5ae33e0e24c34b23082709cb8125a93b3aa5c
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-09-23 15:21:57 +0700

    Create authentication middleware

---
+++ server/src/middlewares/authentication.middleware.js
@@ -0,0 +1,24 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user.model')
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: false
+}, (email, password, done) => User.findOne({ email: email })
+  .then(async user => user
+    ? await user.authenticate(password)
+      ? done(null, user)
+      : done(null, false, 'Wrong password.')
+    : done(null, false, 'Username not registered.'))
+  .catch(err => done(err, false, 'Error'))
+))
+
+passport.serializeUser((user, done) =>
+  process.nextTick(() => done(null, user.sign())))
+
+passport.deserializeUser((user, done) =>
+  process.nextTick(() => done(null, user)))
+
+module.exports = passport.authenticate('session')
