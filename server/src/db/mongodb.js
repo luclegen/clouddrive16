f49ca4ed5b783542e93664d4d92cfe42df73b639
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-09-26 16:06:11 +0700

    Add @casl/mongoose

--- server/src/db/mongodb.js
+++ server/src/db/mongodb.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
+const { accessibleRecordsPlugin } = require('@casl/mongoose')
 const { info, error } = require('console')
 const { NODE_ENV } = process.env
 
 mongoose.connect(process.env.MONGODB)
   .then(() => info(`Connected to ${NODE_ENV === 'production' ? 'Database' : 'MongoDB'}.`))
   .catch(err => error('Could not connect to MongoDB:', err))
+
+mongoose.plugin(accessibleRecordsPlugin)
