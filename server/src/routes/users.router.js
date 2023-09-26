dcee90ee12b141cad5e7aeea4a67b7ae35708717
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-09-26 08:06:56 +0700

    Add changeLang

--- server/src/routes/users.router.js
+++ server/src/routes/users.router.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const authorize = require('../middlewares/authorization.middleware');
+const authorize = require('../middlewares/authorization.middleware')
 const userController = require('../controllers/users.controller')
 
 router.post('/', userController.create)
 router.get('/', authorize, userController.read)
+router.patch('/', authorize, userController.changeLang)
 
 module.exports = router
