654450ca60a0a6b356727dd312948aecb34a2162
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-09-27 09:22:24 +0700

    Add defineAbilitiesFor

--- server/src/middlewares/authorization.middleware.js
+++ server/src/middlewares/authorization.middleware.js
@@ -1,8 +1,24 @@
-// const { AbilityBuilder, createMongoAbility, ForbiddenError } = require('@casl/ability')
+const { AbilityBuilder, createMongoAbility } = require('@casl/ability')
 const jwt = require('jsonwebtoken')
 const util = require('util')
 const catchAsync = require('../middlewares/catcher.middleware')
 const User = require('../models/user.model')
+const roles = require('../models/roles.json')
+
+const defineAbilitiesFor = user => {
+  const { can, cannot, rules } = new AbilityBuilder(createMongoAbility)
+
+  for (const role of user.roles) {
+    const permissions = roles[role]
+    for (const p of permissions) {
+      if (p?.fields) { p.fields = Object.fromEntries(Object.entries(p?.fields).map((v, i) => [v[0], v[1] === 'mine' ? user._id : v[1]])) }
+      if (p.can) can(p.action, p.subject, p.fields)
+      else cannot(p.action, p.subject, p.fields)
+    }
+  }
+
+  return new createMongoAbility(rules)
+}
 
 module.exports = catchAsync(async (req, res, next) => {
   req.i18n.changeLanguage(req.cookies?.lang)
@@ -10,6 +26,7 @@ module.exports = catchAsync(async (req, res, next) => {
   if (req.session?.passport?.user) {
     req.payload = await util.promisify(jwt.verify)(req.session?.passport?.user, process.env.SECRET)
     req.user = await User.findById(req.payload)
+    req.ability = defineAbilitiesFor(req.user || null)
   	next()
   } else {
     res
@@ -21,6 +38,6 @@ module.exports = catchAsync(async (req, res, next) => {
       .clearCookie('middle_name')
       .clearCookie('last_name')
       .status(401)
-      .json(req.t('Unauthorized'))
+      .json(req.t('Unauthorized.'))
   }
 })
