7c808c2d8abf1162a3432e4b51b9a04c3c0d4681
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-01 10:02:05 +0700

    Add changePassword

--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/auth.controller')
 router.post('/', authController.login)
 router.get('/:email', authController.available)
 router.put('/', authorize, authController.verify)
+router.patch('/', authorize, authController.changePassword)
 router.delete('/', authorize, authController.logout)
 
 module.exports = router
