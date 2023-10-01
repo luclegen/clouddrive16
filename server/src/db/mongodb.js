f1820f259149824657ff927902d1d2c685df1f94
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-01 11:22:52 +0700

    Add accessibleFieldsPlugin

--- server/src/db/mongodb.js
+++ server/src/db/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { accessibleRecordsPlugin } = require('@casl/mongoose')
+const { accessibleRecordsPlugin, accessibleFieldsPlugin } = require('@casl/mongoose')
 const { info, error } = require('console')
 const { NODE_ENV } = process.env
 
@@ -7,4 +7,6 @@ mongoose.connect(process.env.MONGODB)
   .then(() => info(`Connected to ${NODE_ENV === 'production' ? 'Database' : 'MongoDB'}.`))
   .catch(err => error('Could not connect to MongoDB:', err))
 
-mongoose.plugin(accessibleRecordsPlugin)
+mongoose
+  .plugin(accessibleRecordsPlugin)
+  .plugin(accessibleFieldsPlugin)
