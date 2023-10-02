fa01ea3e61dd92d106e08df7cd7cb77981649da8
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-02 15:31:21 +0700

    Optimize auth

--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const authorize = require('../middlewares/authorization.middleware')
 const authController = require('../controllers/auth.controller')
 
-router.post('/', authController.login)
-router.get('/:email', authController.available)
-router.put('/', authorize, authController.verify)
-router.patch('/', authorize, authController.changePassword)
-router.delete('/', authorize, authController.logout)
+router
+  .post('/', authController.login)
+  .get('/:email', authController.available)
+  .put('/', authorize, authController.verify)
+  .patch('/', authorize, authController.changePassword)
+  .delete('/', authorize, authController.logout)
 
 module.exports = router
