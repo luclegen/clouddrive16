753e1a5de52e733161d06df59b7106df22ef3ccf
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-03 08:33:52 +0700

    Remove authorization middleware

--- server/src/routes/folders.router.js
+++ server/src/routes/folders.router.js
@@ -1,15 +1,15 @@
 const router = require('express').Router()
-const authorize = require('../middlewares/authorization.middleware');
 const foldersController = require('../controllers/folders.controller')
 
-router.post('/', authorize, foldersController.create)
-router.get('/:id', authorize, foldersController.read)
-router.put('/:id', authorize, foldersController.update)
-router.patch('/:id', authorize, foldersController.delete)
-router.patch('/r/:id', authorize, foldersController.restore)
-router.patch('/m/:id', authorize, foldersController.move)
-router.patch('/c/:id', authorize, foldersController.copy)
-router.delete('/:id', authorize, foldersController.deleteForever)
-router.get('/', authorize, foldersController.list)
+router
+  .post('/', foldersController.create)
+  .get('/:id', foldersController.read)
+  .put('/:id', foldersController.update)
+  .patch('/:id', foldersController.delete)
+  .patch('/r/:id', foldersController.restore)
+  .patch('/m/:id', foldersController.move)
+  .patch('/c/:id', foldersController.copy)
+  .delete('/:id', foldersController.deleteForever)
+  .get('/', foldersController.list)
 
 module.exports = router
