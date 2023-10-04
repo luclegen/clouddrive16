ab492707964a9df688e2460f8bcbad4255308f53
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-04 08:11:47 +0700

    Add type to toUploadPath and update toPath

--- server/src/helpers/converter.js
+++ server/src/helpers/converter.js
@@ -2,9 +2,9 @@ const duplicator = require('./duplicator')
 
 module.exports.capitalize = text => text.length > 0 ? text[0].toUpperCase() + text.slice(1) : text
 
-module.exports.toPath = (value, array) => value.path + (array?.length ? duplicator.copyFolderInFolder(value.name, array) : value.name) + '/'
+module.exports.toPath = (value, array) => `${value.path}${array?.length ? duplicator.copyFolderInFolder(value.name, array) : value.name}/`
 
-module.exports.toUploadPath = (id, value, array) => process.env.UPLOADS + id + '/files' + (array?.length ? this.toPath(value, array) : this.toPath(value))
+module.exports.toUploadPath = (id, value, array, type = 'private') => `${process.env.UPLOADS}${type}/${id}/files${array?.length ? this.toPath(value, array) : this.toPath(value)}`
 
 module.exports.toFilePath = (value, array) => value.path + (array?.length ? duplicator.copyFolderInFolder(value.name, array) : value.name)
 
