8d433767c94f00113df1d6aae0dc6acd0dd84262
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-05 07:54:33 +0700

    Move ability up folder.save()

--- server/src/controllers/folders.controller.js
+++ server/src/controllers/folders.controller.js
@@ -20,11 +20,11 @@ module.exports.create = catchAsync(async (req, res, next) => {
     folder.path = req.body.path
     folder.name = req.body.name
 
+    if (folder) ForbiddenError.from(req.ability).throwUnlessCan('create', folder)
+
     folder = await folder.save()
 
     if (folder) {
-      ForbiddenError.from(req.ability).throwUnlessCan('create', folder)
-
       const path = [process.env.UPLOADS]
 
       path[1] = path[0] + 'private'
