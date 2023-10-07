6f5e5ba09fdf2d34ef894e0af16cb7cdc59ed723
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-07 07:52:12 +0700

    Optimize create

--- server/src/controllers/codes.controller.js
+++ server/src/controllers/codes.controller.js
@@ -1,31 +1,25 @@
 const { ForbiddenError } = require('@casl/ability')
 const nodemailer = require('nodemailer')
-const createError = require('http-errors')
-const User = require('../models/user.model')
 const Code = require('../models/code.model')
 const generator = require('../helpers/generator')
 const mailer = require('../helpers/mailer')
 const catchAsync = require('../middlewares/catcher.middleware')
 
 module.exports.create = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.user).accessibleBy(req.ability)
+  let code = await Code.findOne({ _uid: req.user })
+  const body = generator.genCode(6)
 
-  if (user) {
-    let code = await Code.findOne({ _uid: user })
-    const body = generator.genCode(6)
+  if (!code) {
+    code = new Code()
+    code._uid = req.user
+  }
 
-    if (!code) {
-      code = new Code()
-      code._uid = user
-    }
+  code.body = body
+  code.attempts = 3
 
-    code.body = body
-    code.attempts = 3
+  ForbiddenError.from(req.ability).throwUnlessCan('create', code)
 
-    code = await code.save()
+  code = await code.save()
 
-    ForbiddenError.from(req.ability).throwUnlessCan('create', code)
-
-    res.status(201).send(req.app.get('env') === 'production' ? nodemailer.getTestMessageUrl(await mailer.sendCode(req.user.email, 'Verify Code', body)) : body)
-  } else next(createError(404, 'User not found.'))
+  res.status(201).send(req.app.get('env') === 'production' ? nodemailer.getTestMessageUrl(await mailer.sendCode(req.user.email, 'Verify Code', body)) : body)
 })
