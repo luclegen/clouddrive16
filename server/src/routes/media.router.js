b9c84e078898e09b487e6a405845a50bb12b5e46
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-09 16:00:44 +0700

    Format media router

--- server/src/routes/media.router.js
+++ server/src/routes/media.router.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-const authorize = require('../middlewares/authorization.middleware');
-const mediaController = require('../controllers/media.controller');
+const router = require('express').Router()
+const authorize = require('../middlewares/authorization.middleware')
+const mediaController = require('../controllers/media.controller')
 
-router.get('/', authorize, mediaController.list);
+router.get('/', authorize, mediaController.list)
 
-module.exports = router;
+module.exports = router
