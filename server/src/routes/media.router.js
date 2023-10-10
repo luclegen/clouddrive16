a2de70fc672f91e875e476d1d04ddb2c5afae09b
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-10 09:11:46 +0700

    Remove media

--- server/src/routes/media.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const authorize = require('../middlewares/authorization.middleware')
-const mediaController = require('../controllers/media.controller')
-
-router.get('/', authorize, mediaController.list)
-
-module.exports = router
