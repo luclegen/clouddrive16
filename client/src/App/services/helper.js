82af8e22085660f4ed9239e3007020dfa83ac756
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-12 20:16:29 +0700

    Add decodeURIComponent to getCookie

--- client/src/App/services/helper.js
+++ client/src/App/services/helper.js
@@ -104,7 +104,7 @@ class Helper {
     for (let i = 0; i < ca.length; i++) {
       let c = ca[i]
       while (c.charAt(0) === ' ') c = c.substring(1, c.length)
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
+      if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length))
     }
     return null
   }
