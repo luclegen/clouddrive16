46734b8ce4cf54e90d269b878c24479e1c99e66d
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-17 11:22:44 +0700

    Add folderTreeReducer

--- client/src/App/store.js
+++ client/src/App/store.js
@@ -1,12 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import appReducer from './slice';
-import headerReducer from './components/Header/slice';
-import homeReducer from './pages/Home/slice';
-import loginReducer from './components/Login/slice';
-import registrationReducer from './components/Registration/slice';
-import activateReducer from './components/Activate/slice';
-import filesReducer from './components/Files/slice';
-import progressReducer from './components/Progress/slice';
+import { configureStore } from '@reduxjs/toolkit'
+import appReducer from './slice'
+import headerReducer from './components/Header/slice'
+import homeReducer from './pages/Home/slice'
+import loginReducer from './components/Login/slice'
+import registrationReducer from './components/Registration/slice'
+import activateReducer from './components/Activate/slice'
+import filesReducer from './components/Files/slice'
+import progressReducer from './components/Progress/slice'
+import folderTreeReducer from './components/FolderTree/slice'
 
 export const store = configureStore({
   reducer: {
@@ -17,6 +18,7 @@ export const store = configureStore({
     registration: registrationReducer,
     activate: activateReducer,
     files: filesReducer,
-    progress: progressReducer
+    progress: progressReducer,
+    folderTree: folderTreeReducer
   }
 });
