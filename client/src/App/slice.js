7e79f6694d75f11db7d5202147ae0b6a524039e8
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-22 07:47:47 +0700

    Reformat

--- client/src/App/slice.js
+++ client/src/App/slice.js
@@ -1,73 +1,61 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import authService from './services/auth';
-import helper from './services/helper';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import authService from './services/auth'
+import helper from './services/helper'
 
 const initialState = {
   available: true,
   remember: false,
   loggedIn: helper.loggedIn(),
   is_activate: helper.getCookie('is_activate') === 'true'
-};
+}
 
-export const check = createAsyncThunk(
-  'app/check',
-  async (email) => (await authService.available(email)).status === 200
-);
-export const login = createAsyncThunk(
-  'app/login',
-  async (user) => (await authService.login(user)).data
-);
-export const logout = createAsyncThunk(
-  'app/logout',
-  async () => (await authService.logout()).data
-);
-export const verify = createAsyncThunk(
-  'app/verify',
-  async (code) => (await authService.verify(code)).data
-);
+export const check = createAsyncThunk('app/check', async (email) => (await authService.available(email)).status === 200)
+export const login = createAsyncThunk('app/login', async (user) => (await authService.login(user)).data)
+export const logout = createAsyncThunk('app/logout', async () => (await authService.logout()).data)
+export const verify = createAsyncThunk('app/verify', async (code) => (await authService.verify(code)).data)
 
 export const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
     setRemember: (state, action) => {
-      state.remember = action.payload;
+      state.remember = action.payload
     },
     setLoggedIn: (state, action) => {
-      state.loggedIn = action.payload;
+      state.loggedIn = action.payload
     }
   },
   extraReducers: (builder) =>
     builder
       .addCase(check.fulfilled, (state, action) => {
-        state.available = action.payload;
+        state.available = action.payload
       })
       .addCase(login.pending, (state) => {
-        state.remember = false;
+        state.remember = false
       })
       .addCase(login.fulfilled, (state, action) => {
         !action.payload.is_activate &&
-          alert('Your session exists for 5 minutes.');
-        state.is_activate = action.payload.is_activate;
-        state.loggedIn = true;
+          alert('Your session exists for 5 minutes.')
+        state.is_activate = action.payload.is_activate
+        state.loggedIn = true
       })
       .addCase(verify.fulfilled, (state) => {
-        state.is_activate = true;
+        state.is_activate = true
       })
       .addCase(verify.rejected, () => {
-        document.querySelector('.form-only').reset();
-        document.querySelector('.form-control-digit').focus();
+        document.querySelector('.form-only').reset()
+        document.querySelector('.form-control-digit').focus()
       })
       .addCase(logout.fulfilled, (state) => {
-        state.loggedIn = false;
+        state.loggedIn = false
       })
-});
+})
 
-export const { setRemember, setLoggedIn } = appSlice.actions;
+export const { setRemember, setLoggedIn } = appSlice.actions
 
-export const selectAvailable = (state) => state.app.available;
-export const selectRemember = (state) => state.app.remember;
-export const selectLoggedIn = (state) => state.app.loggedIn;
-export const selectIsActivate = (state) => state.app.is_activate;
+export const selectAvailable = (state) => state.app.available
+export const selectRemember = (state) => state.app.remember
+export const selectLoggedIn = (state) => state.app.loggedIn
+export const selectIsActivate = (state) => state.app.is_activate
 
-export default appSlice.reducer;
+export default appSlice.reducer
