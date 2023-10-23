4c898f686b7dec8ca87d9b45ad9bc42654651abb
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-23 09:46:30 +0700

    Reformat handler middleware

--- server/src/middlewares/handler.middleware.js
+++ server/src/middlewares/handler.middleware.js
@@ -9,50 +9,50 @@ module.exports = async (err, req, res, next) => {
   if (req.file && status !== 422) fs.rmSync(req.file.path)
 
   switch (err.name) {
-  case 'ValidationError':
-    status = 400
-    msg = Object.values(err.errors).map((e, i) => (i + 1) + '. ' + req.t(e)).join(';\n') + '.'
-    break
-
-  case 'JsonWebTokenError':
-    status = 400
-    break
-
-  case 'TokenExpiredError':
-    status = 401
-    break
-
-  case 'ForbiddenError':
-    status = 403
-    break
-
-  case 'AxiosError':
-    status = 501
-    msg = err.response?.data?.response || err.response?.data || err.message
-    break
-
-  default:
-    switch (err.code) {
-    case 11000:
-      status = 422
-      msg = converter.capitalize(Object.keys(err.keyValue)[0] + ' is taken!')
+    case 'ValidationError':
+      status = 400
+      msg = Object.values(err.errors).map((e, i) => (i + 1) + '. ' + req.t(e)).join(';\n') + '.'
       break
 
-    case 'ENOENT':
-    case 'ENOTDIR':
-    case 'ENOTEMPTY':
+    case 'JsonWebTokenError':
+      status = 400
+      break
+
+    case 'TokenExpiredError':
+      status = 401
+      break
+
+    case 'ForbiddenError':
+      status = 403
+      break
+
+    case 'AxiosError':
       status = 501
-      msg = err.message
-      break
-
-    case 'ECONNREFUSED':
-      status = 503
-      msg = err.message
+      msg = err.response?.data?.response || err.response?.data || err.message
       break
 
     default:
-      return res.status(status).json(req.t(converter.capitalize(msg)))
-    }
+      switch (err.code) {
+        case 11000:
+          status = 422
+          msg = converter.capitalize(Object.keys(err.keyValue)[0] + ' is taken!')
+          break
+
+        case 'ENOENT':
+        case 'ENOTDIR':
+        case 'ENOTEMPTY':
+          status = 501
+          msg = err.message
+          break
+
+        case 'ECONNREFUSED':
+          status = 503
+          msg = err.message
+          break
+
+        default:
+          return res.status(status).json(req.t(converter.capitalize(msg)))
+      }
   }
 
   return res.status(status).json(req.t(converter.capitalize(msg)))
