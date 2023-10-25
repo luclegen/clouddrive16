d934f973f65c80d0a3cb51103c5ebb373281b5ae
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-10-25 14:58:27 +0700

    Add profileReducer, informationReducer, and avatarCropperReducer

--- client/src/App/store.js
+++ client/src/App/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import appReducer from './slice'
-import headerReducer from './components/Header/slice'
 import homeReducer from './pages/Home/slice'
+import profileReducer from './pages/Profile/slice'
+import informationReducer from './components/Information/slice'
+import headerReducer from './components/Header/slice'
 import loginReducer from './components/Login/slice'
 import registrationReducer from './components/Registration/slice'
 import activateReducer from './components/Activate/slice'
@@ -9,18 +11,22 @@ import filesReducer from './components/Files/slice'
 import progressReducer from './components/Progress/slice'
 import folderTreeReducer from './components/FolderTree/slice'
 import mediaReducer from './components/Media/slice'
+import avatarCropperReducer from './components/AvatarCropper/slice'
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
     header: headerReducer,
     home: homeReducer,
+    profile: profileReducer,
+    information: informationReducer,
     login: loginReducer,
     registration: registrationReducer,
     activate: activateReducer,
     files: filesReducer,
     progress: progressReducer,
     folderTree: folderTreeReducer,
-    media: mediaReducer
+    media: mediaReducer,
+    avatarCropper: avatarCropperReducer
   }
 });
