a0ee601bc7c984a593769da7155b5752cf289c33
Luc Tan Huynh <luclegen.dev@gmail.com>
2023-11-03 15:30:47 +0700

    Create search, setKeyword, setFoundFolders, setFoundFiles

--- client/src/App/components/Header/slice.js
+++ client/src/App/components/Header/slice.js
@@ -1,4 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import foldersService from '../../services/folders'
+import filesService from '../../services/files'
 
 const initialState = {
   width: window.innerWidth,
@@ -7,18 +9,11 @@ const initialState = {
   opened: false,
   isOpen: false,
   foundFolders: [],
-  foundFiles: []
+  foundFiles: [],
+  keyword: ''
 }
 
-export const search = createAsyncThunk(
-  'counter/search',
-  async keyword => {
-    console.log(keyword)
-    // const response = await fetchCount(amount)
-    // The value we return becomes the `fulfilled` action payload
-    // return response.data
-  }
-)
+export const search = createAsyncThunk('header/search', async (keyword = '') => ({ folders: (await foldersService.list(keyword)).data, files: (await filesService.list(keyword)).data }))
 
 export const headerSlice = createSlice({
   name: 'header',
@@ -28,8 +23,16 @@ export const headerSlice = createSlice({
     setOpened: (state, action) => { state.opened = action.payload },
     toggleDropdown: state => { state.isOpen = !state.isOpen },
     hideDropdown: state => { state.isOpen && (state.isOpen = false) },
-    setWidth: state => { state.width = window.innerWidth }
-  }
+    setWidth: state => { state.width = window.innerWidth },
+    setFoundFolders: (state, action) => { state.foundFolders = action.payload },
+    setFoundFiles: (state, action) => { state.foundFiles = action.payload },
+    setKeyword: (state, action) => { state.keyword = action.payload }
+  },
+  extraReducers: builder => builder
+    .addCase(search.fulfilled, (state, action) => {
+      if (state.keyword) [state.foundFolders, state.foundFiles] = Object.values(action.payload)
+      else[state.foundFolders, state.foundFiles] = []
+    })
 })
 
 export const {
@@ -37,13 +40,17 @@ export const {
   toggleDropdown,
   hideDropdown,
   setOpened,
-  setWidth
+  setWidth,
+  setKeyword,
+  setFoundFolders,
+  setFoundFiles
 } = headerSlice.actions
 
 export const selectHover = state => state.header.hover
 export const selectWidth = state => state.header.width
 export const selectOpened = state => state.header.opened
 export const selectIsOpen = state => state.header.isOpen
+export const selectKeyword = state => state.header.keyword
 export const selectFoundFolders = state => state.header.foundFolders
 export const selectFoundFiles = state => state.header.foundFiles
 
